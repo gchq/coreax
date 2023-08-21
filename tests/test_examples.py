59709ff0549006ee124afbd66a3e61c05f77ba20
sk062380 <138580305+sk062380@users.noreply.github.com>
2023-08-21 15:15:17 +0100

    Add some extra comments

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -31,30 +31,38 @@ class TestExamples(unittest.TestCase):
     def test_david(self):
         """
         Test david.py example
+
+        Primarily an end-to-end test to check david.py runs without error.
         """
 
+        # assign a temporary output directory and patch the print() and pyplot.show() functions
         with tempfile.TemporaryDirectory() as tmp_dir, \
                 patch('builtins.print') as mock_print, \
                 patch("matplotlib.pyplot.show") as mock_show:
 
-                # run david.py
-                inpath_ = Path.cwd().parent / Path("examples/data/david_orig.png")
-                outpath_ = Path(tmp_dir) / 'david_coreset.png'
-                d(inpath=str(inpath_), outpath=outpath_)
+            # run david.py
+            inpath_ = Path.cwd().parent / Path("examples/data/david_orig.png")
+            outpath_ = Path(tmp_dir) / 'david_coreset.png'
+            d(inpath=str(inpath_), outpath=outpath_)
 
-                mock_print.assert_has_calls([call("Image dimensions:"),
-                                             call((215, 180)),
-                                             call("Computing coreset..."),
-                                             call("Choosing random subset..."),
-                                             call("Plotting")])
+            # check the calls to print mainly to check the loaded image size
+            mock_print.assert_has_calls([call("Image dimensions:"),
+                                         call((215, 180)),
+                                         call("Computing coreset..."),
+                                         call("Choosing random subset..."),
+                                         call("Plotting")])
 
-                mock_show.assert_called_once()
+            # check that the patched plt.show has been called once
+            mock_show.assert_called_once()
 
-                assertisfile(outpath_)
+            # check the file was generated and saved
+            assertisfile(outpath_)
 
     def test_pounce(self):
         """
         Test pounce.py example
+
+        Primarily an end-to-end test to check pounce.py runs without error.
         """
 
         dir_ = Path.cwd().parent / Path("examples/data/pounce")
@@ -66,25 +74,33 @@ class TestExamples(unittest.TestCase):
                 if sub.name in ["coreset.gif", "frames.png"]:
                     sub.unlink()
 
+        # patch the print() function
         with patch('builtins.print'):
             # run pounce.py
             p(dir_=str(dir_))
 
+            # check files were generated and saved
             assertisfile(dir_ / Path('coreset/coreset.gif'))
             assertisfile(dir_ / Path('coreset/frames.png'))
 
     def test_weighted_herding(self):
         """
         Test weighted_herding.py example
+
+        Primarily an end-to-end test to check weighted_herding.py runs without error.
         """
 
+        # assign a temporary output directory and patch the print() and pyplot.show() functions
         with tempfile.TemporaryDirectory() as tmp_dir, \
                 patch('builtins.print'), \
                 patch("matplotlib.pyplot.show") as mock_show:
 
+            # run weighted herding example
             outpath_ = Path(tmp_dir) / 'weighted_herding.png'
             wh(outpath=outpath_)
 
+            # check that the patched plt.show has been called twice
             mock_show.assert_has_calls([call(), call()])
 
+            # check a plot file has been generated
             assertisfile(outpath_)
