8fa1988c27a4902d15325d4ffecfdfe23ee09cad
tl82649 <tl82649>
2023-08-22 15:31:40 +0000

    score matching formatting updated with type annotations, black and 88 char wrap; references added to .bib. A few more unit tests remaining

--- coreax/score_matching.py
+++ coreax/score_matching.py
@@ -1,186 +1,240 @@
 # © Crown Copyright GCHQ
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+# file except in compliance with the License. You may obtain a copy of the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
 
 import jax
 from jax import random, vmap, numpy as jnp, jvp, jit
+from jax.random import PRNGKey
 from jax.lax import fori_loop
-from flax.training import train_state
+from jax.typing import ArrayLike
+from typing import Callable, Optional
+from flax.linen import Module
+from flax.training.train_state import TrainState
 from functools import partial
 from coreax.networks import ScoreNetwork
 import optax
 from tqdm import tqdm
 
-# analytic (reduced variance) loss, for use with certain random measures, e.g. normal and Rademacher.
-analytic_obj = jit(lambda v, u, s: v @ u + .5 * s @ s)
+# analytic (reduced variance) loss, for use with certain random measures, e.g. normal
+# and Rademacher.
+analytic_obj = jit(lambda v, u, s: v @ u + 0.5 * s @ s)
+
 # general loss, for use with general random measures.
-general_obj = jit(lambda v, u, s: v @ u + .5 * (v @ s)**2)
+general_obj = jit(lambda v, u, s: v @ u + 0.5 * (v @ s) ** 2)
 
 
 @partial(jit, static_argnames=["score_network", "obj_fn"])
-def sliced_score_matching_loss_element(x, v, score_network, obj_fn):
-    """Element-wise loss function computation. Computes the loss function from Section 3.2 of 
-    Song el al.'s paper on sliced score matching: https://arxiv.org/pdf/1905.07088.pdf.
+def sliced_score_matching_loss_element(
+    x: ArrayLike, v: ArrayLike, score_network: Callable, obj_fn: Callable
+) -> float:
+    """Element-wise loss function computation.
 
-    Note, this function assumes Gaussian or Rademacher variables, so it uses the L2 norm squared.
+    Computes the loss function from Section 3.2 of Song el al.'s paper on sliced score
+    matching [ssm]_
 
-    Args:
-        x (arraylike): d-dimensional data vector
-        v (arraylike): d-dimensional random vector
-        score_network (callable): function that calls the neural network on x
-        obj_fn (callable): function (v, u, s)
-
-    Returns:
-        float: objective function for single x and v.
+    :param x: d-dimensional data vector
+    :param v: d-dimensional random vector
+    :param score_network: function that calls the neural network on x
+    :param obj_fn: objective function with arguments (v, u, s) -> real
+    :return: objective function output for single x and v inputs
     """
     s, u = jvp(score_network, (x,), (v,))
     return obj_fn(v, u, s)
 
 
-def sliced_score_matching_loss(score_network, obj_fn):
-    """Vector mapped loss function for application to arbitrary numbers of X and V vectors.
+def sliced_score_matching_loss(score_network: Callable, obj_fn: Callable) -> Callable:
+    """Vector mapped loss function for application to arbitrary numbers of X and V
+    vectors.
 
-    Args:
-        score_network (callable): function that calls the neural network on x
-        obj_fn (callable): element-wise function (vector, vector, score_network) -> real
-        obj_fn (callable): objective function (vector, vector, vector) -> real
-
-    Returns:
-        callable: vectorised sliced score matching loss function
+    :param score_network: function that calls the neural network on x
+    :param obj_fn: element-wise function (vector, vector, score_network) -> real
+    :return: callable vectorised sliced score matching loss function
     """
-    inner = vmap(lambda x, v: sliced_score_matching_loss_element(
-        x, v, score_network, obj_fn), (None, 0), 0)
+    inner = vmap(
+        lambda x, v: sliced_score_matching_loss_element(x, v, score_network, obj_fn),
+        (None, 0),
+        0,
+    )
     return vmap(inner, (0, 0), 0)
 
 
-def create_train_state(module, rng, learning_rate, dimension, optimiser):
-    """Creates a flax TrainState for learning with 
+def create_train_state(
+    module: Module,
+    rng: PRNGKey,
+    learning_rate: float,
+    dimension: int,
+    optimiser: Callable,
+) -> TrainState:
+    """Creates a flax TrainState for learning with
 
-    Args:
-        module (flax.nn.Module): flax network class that inherits flax.nn.Module
-        rng (jax.random.PRNGKey): random number generator
-        learning_rate (float): optimiser learning rate
-        dimension (int): data dimension 
-        optimiser (callable): optax optimiser, e.g. optax.adam
-
-    Returns:
-        flax.train_state.TrainState: TrainState object
+    :param module: flax network class that inherits flax.nn.Module
+    :param rng: random number generator
+    :param learning_rate: optimiser learning rate
+    :param dimension: data dimension
+    :param optimiser: optax optimiser, e.g. optax.adam
+    :return: TrainState object
     """
-    params = module.init(rng, jnp.ones((1, dimension)))['params']
+    params = module.init(rng, jnp.ones((1, dimension)))["params"]
     tx = optimiser(learning_rate)
-    return train_state.TrainState.create(apply_fn=module.apply, params=params, tx=tx)
+    return TrainState.create(apply_fn=module.apply, params=params, tx=tx)
 
 
 @partial(jit, static_argnames=["obj_fn"])
-def sliced_score_matching_train_step(state, X, V, obj_fn):
+def sliced_score_matching_train_step(
+    state: TrainState, X: ArrayLike, V: ArrayLike, obj_fn: Callable
+) -> TrainState:
     """A single training step that updates model parameters using loss gradient.
 
-    Args:
-        state (flax.train_state.TrainState): the TrainState object.
-        X (arraylike): the n x d data vectors
-        V (arraylike): the n x m x d random vectors
-        obj_fn (callable): objective function (vector, vector, vector) -> real
-
-    Returns:
-        flax.train_state.TrainState: the updated TrainState object 
+    :param state: the TrainState object.
+    :param X: the n x d data vectors
+    :param V: the n x m x d random vectors
+    :param obj_fn: objective function (vector, vector, vector) -> real
+    :return: the updated TrainState object
     """
-    def loss(params): return sliced_score_matching_loss(
-        lambda x: state.apply_fn({'params': params}, x), obj_fn)(X, V).mean()
+
+    def loss(params):
+        return sliced_score_matching_loss(
+            lambda x: state.apply_fn({"params": params}, x), obj_fn
+        )(X, V).mean()
+
     grads = jax.grad(loss)(state.params)
     state = state.apply_gradients(grads=grads)
     return state
 
 
 @partial(jit, static_argnames=["obj_fn"])
-def noise_conditional_loop_body(i, obj, state, params, X, V, sigmas, obj_fn):
-    """Noise conditioning objective function summand. See https://arxiv.org/pdf/2006.09011.pdf for details.
+def noise_conditional_loop_body(
+    i: int,
+    obj: float,
+    state: TrainState,
+    params: dict,
+    X: ArrayLike,
+    V: ArrayLike,
+    sigmas: ArrayLike,
+    obj_fn: Callable,
+) -> float:
+    """Noise conditioning objective function summand.
 
-    Args:
-        i (int): loop index
-        obj (float): running objective, i.e. the current partial sum
-        state (flax.train_state.TrainState): the TrainState object
-        params (dict): the current iterate parameter settings
-        X (arraylike): the n x d data vectors
-        V (arraylike): the n x m x d random vectors
-        sigmas (arraylike): the geometric progression of noise standard deviations
-        obj_fn (callable): element objective function (vector, vector, vector) -> real
+    See [improvedsgm]_ for details.
 
-    Returns:
-        float: the updated objective, i.e. partial sum
+    :param i: loop index
+    :param obj: running objective, i.e. the current partial sum
+    :param state: the TrainState object
+    :param params: the current iterate parameter settings
+    :param X: the n x d data vectors
+    :param V: the n x m x d random vectors
+    :param sigmas: the geometric progression of noise standard deviations
+    :param obj_fn: element objective function (vector, vector, vector) -> real
+    :return: the updated objective, i.e. partial sum
     """
     # perturb X
     X_ = X + sigmas[i] * random.normal(random.PRNGKey(0), X.shape)
-    obj = obj + sigmas[i]**2 * sliced_score_matching_loss(
-        lambda x: state.apply_fn({'params': params}, x), obj_fn)(X_, V).mean()
+    obj = (
+        obj
+        + sigmas[i] ** 2
+        * sliced_score_matching_loss(
+            lambda x: state.apply_fn({"params": params}, x), obj_fn
+        )(X_, V).mean()
+    )
     return obj
 
 
 @partial(jit, static_argnames=["obj_fn", "L"])
-def noise_conditional_train_step(state, X, V, obj_fn, sigmas, L):
+def noise_conditional_train_step(
+    state: TrainState,
+    X: ArrayLike,
+    V: ArrayLike,
+    obj_fn: Callable,
+    sigmas: ArrayLike,
+    L: int,
+) -> TrainState:
     """A single training step that updates model parameters using loss gradient.
 
-    Args:
-        state (flax.train_state.TrainState): the TrainState object.
-        X (arraylike): the n x d data vectors
-        V (arraylike): the n x m x d random vectors
-        obj_fn (callable): objective function (vector, vector, vector) -> real
-        sigmas (arraylike): length L array of noise standard deviations to use in objective function.
-        L (int): the static number of terms in the geometric progression. (Required for reverse mode autodiff.)
-
-    Returns:
-        flax.train_state.TrainState: the updated TrainState object 
+    :param state: the TrainState object.
+    :param X: the n x d data vectors
+    :param V: the n x m x d random vectors
+    :param obj_fn: objective function (vector, vector, vector) -> real
+    :param sigmas: length L array of noise standard deviations to use in objective
+        function.
+    :param L: the static number of terms in the geometric progression. (Required for
+        reverse mode autodiff.)
+    :return: the updated TrainState object
     """
+
     def loss(params):
-        body = partial(noise_conditional_loop_body, state=state,
-                       params=params, X=X, V=V, sigmas=sigmas, obj_fn=obj_fn)
-        return fori_loop(0, L, body, 0.)
+        body = partial(
+            noise_conditional_loop_body,
+            state=state,
+            params=params,
+            X=X,
+            V=V,
+            sigmas=sigmas,
+            obj_fn=obj_fn,
+        )
+        return fori_loop(0, L, body, 0.0)
+
     grads = jax.grad(loss)(state.params)
     state = state.apply_gradients(grads=grads)
     return state
 
 
-def sliced_score_matching(X, rgenerator, noise_conditioning=True, use_analytic=False, M=1, lr=1e-3, epochs=10, batch_size=64, hidden_dim=128, optimiser=optax.adam, L=100, sigma=1., gamma=.95):
-    """Learn a sliced score matching function from Song et al.'s paper: https://arxiv.org/pdf/1905.07088.pdf.
+def sliced_score_matching(
+    X: ArrayLike,
+    rgenerator: Callable,
+    noise_conditioning: Optional[bool] = True,
+    use_analytic: Optional[bool] = False,
+    M: Optional[int] = 1,
+    lr: Optional[float] = 1e-3,
+    epochs: Optional[int] = 10,
+    batch_size: Optional[int] = 64,
+    hidden_dim: Optional[int] = 128,
+    optimiser: Optional[Callable] = optax.adam,
+    L: Optional[int] = 100,
+    sigma: Optional[float] = 1.0,
+    gamma: Optional[float] = 0.95,
+) -> Callable:
+    """Learn a sliced score matching function from Song et al.'s paper [ssm]_
 
     Currently uses the ScoreNetwork network in coreax.networks.
 
-    Args:
-        X (arraylike): the n x d data vectors
-        rgenerator (callable): distribution sampler (key, shape, dtype) -> jax._src.typing.Array, e.g. distributions in jax.random
-        noise_conditioning (bool, optional): use the noise conditioning version of score matching. Defaults to True.
-        use_analytic (bool, optional): use the analytic (reduced variance) objective or not. Defaults to False.
-        M (int, optional): the number of random vectors to use per data vector. Defaults to 1.
-        lr (float, optional): optimiser learning rate. Defaults to 1e-3.
-        epochs (int, optional): epochs for training. Defaults to 10.
-        batch_size (int, optional): size of minibatch. Defaults to 64.
-        hidden_dim (int, optional): the ScoreNetwork hidden dimension. Defaults to 128.
-        optimiser (callable, optional): the optax optimiser to use. Defaults to optax.adam.
-        L (int, optional): number of noise models to use in noise conditional score matching. Defaults to 100.
-        sigma (float, optional): initial noise standard deviation for noise geometric progression in noise conditional score matching. Defaults to 1.
-        gamma (float, optional); geometric progression ratio. Defaults to 0.95.
-
-    Returns:
-        callable: a function that applies the learned score function to input x
+    :param X: the n x d data vectors
+    :param rgenerator: distribution sampler (key, shape, dtype) ->
+        jax._src.typing.Array, e.g. distributions in jax.random
+    :param noise_conditioning: use the noise conditioning version of score matching.
+        Defaults to True.
+    :param use_analytic: use the analytic (reduced variance) objective or not. Defaults
+        to False.
+    :param M: the number of random vectors to use per data vector. Defaults to 1.
+    :param lr: optimiser learning rate. Defaults to 1e-3.
+    :param epochs: epochs for training. Defaults to 10.
+    :param batch_size: size of minibatch. Defaults to 64.
+    :param hidden_dim: the ScoreNetwork hidden dimension. Defaults to 128.
+    :param optimiser: the optax optimiser to use. Defaults to optax.adam.
+    :param L: number of noise models to use in noise conditional score matching.
+        Defaults to 100.
+    :param sigma: initial noise standard deviation for noise geometric progression in
+        noise conditional score matching. Defaults to 1.
+    :param gamma: geometric progression ratio. Defaults to 0.95.
+    :return: a function that applies the learned score function to input x
     """
     # main objects
     n, d = X.shape
     sn = ScoreNetwork(hidden_dim, d)
     obj_fn = analytic_obj if use_analytic else general_obj
     if noise_conditioning:
-        gammas = gamma**jnp.arange(L)
+        gammas = gamma ** jnp.arange(L)
         sigmas = sigma * gammas
-        train_step = partial(noise_conditional_train_step,
-                             obj_fn=obj_fn, sigmas=sigmas, L=L)
+        train_step = partial(
+            noise_conditional_train_step, obj_fn=obj_fn, sigmas=sigmas, L=L
+        )
     else:
         train_step = partial(sliced_score_matching_train_step, obj_fn=obj_fn)
 
@@ -196,4 +250,4 @@ def sliced_score_matching(X, rgenerator, noise_conditioning=True, use_analytic=F
     for _ in tqdm(range(epochs)):
         idx = random.randint(batch_key, (batch_size,), 0, n)
         state = train_step(state, X[idx, :], V[idx, :])
-    return lambda x: state.apply_fn({'params': state.params}, x)
+    return lambda x: state.apply_fn({"params": state.params}, x)
--- tests/test_score_matching.py
+++ tests/test_score_matching.py
@@ -1,47 +1,46 @@
 # © Crown Copyright GCHQ
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+# file except in compliance with the License. You may obtain a copy of the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
 import unittest
 import numpy as np
-from jax import grad, jacfwd, random, numpy as jnp
+from jax import random, numpy as jnp
+from jax.typing import ArrayLike
 from optax import sgd
 
 from coreax.score_matching import *
-from flax.training.train_state import TrainState
 from flax import linen as nn
 import matplotlib.pyplot as plt
 
+
 class TestNetwork(nn.Module):
     """A network for use in sliced score matching."""
+
     hidden_dim: int
     output_dim: int
 
     @nn.compact
-    def __call__(self, x):
+    def __call__(self, x: ArrayLike) -> ArrayLike:
         x = nn.Dense(self.hidden_dim)(x)
         return x
 
-class TestKernels(unittest.TestCase):
 
+class TestScoreMatching(unittest.TestCase):
     def test_analytic_objective(self):
-        """Tests the core objective function, analytic version
-        """
+        """Tests the core objective function, analytic version"""
         # orthogonal u and v vectors should give back half length squared s
-        u = np.array([0., 1.])
-        v = np.array([1., 0.])
+        u = np.array([0.0, 1.0])
+        v = np.array([1.0, 0.0])
         s = np.ones(2, dtype=float)
         out = analytic_obj(v, u, s)
-        ans = 1.
+        ans = 1.0
         self.assertAlmostEqual(out, ans, places=3)
 
         # basic test
@@ -49,18 +48,18 @@ class TestKernels(unittest.TestCase):
         v = np.arange(3, 6, dtype=float)
         s = np.arange(9, 12, dtype=float)
         out = analytic_obj(v, u, s)
-        ans = 165.
+        ans = 165.0
         self.assertAlmostEqual(out, ans, places=3)
 
     def test_general_objective(self):
-        """Tests the core objective function.
-        """
-        # orthogonal u and v vectors should give back half squared dot product of v and s
-        u = np.array([0., 1.])
-        v = np.array([1., 0.])
+        """Tests the core objective function."""
+        # orthogonal u and v vectors should give back half squared dot product of v and
+        # s
+        u = np.array([0.0, 1.0])
+        v = np.array([1.0, 0.0])
         s = np.ones(2, dtype=float)
         out = general_obj(v, u, s)
-        ans = .5
+        ans = 0.5
         self.assertAlmostEqual(out, ans, places=3)
 
         # basic test
@@ -68,17 +67,19 @@ class TestKernels(unittest.TestCase):
         v = np.arange(3, 6, dtype=float)
         s = np.arange(9, 12, dtype=float)
         out = general_obj(v, u, s)
-        ans = 7456.
+        ans = 7456.0
         self.assertAlmostEqual(out, ans, places=3)
 
     def test_sliced_score_matching_loss_element(self):
         # basic score function, implictly multivariate vector valued
-        score_fn = lambda x: x**2
+        def score_fn(x):
+            return x**2
+
         # arbitrary input
-        x = np.array([2., 7.]) 
+        x = np.array([2.0, 7.0])
         s = score_fn(x)
         # Hessian (grad of score function)
-        H = 2. * np.diag(x)
+        H = 2.0 * np.diag(x)
         # arbitrary random vector
         v = np.ones(2, dtype=float)
         out = sliced_score_matching_loss_element(x, v, score_fn, analytic_obj)
@@ -88,29 +89,31 @@ class TestKernels(unittest.TestCase):
 
     def test_sliced_score_matching_loss(self):
         # basic score function, implictly multivariate vector valued
-        score_fn = lambda x: x**2
-        x = np.array([2., 7.])
+        def score_fn(x):
+            return x**2
+
+        x = np.array([2.0, 7.0])
         # arbitrary number of inputs
         X = np.tile(x, (10, 1))
         # arbitrary number of randoms, 1 per input
         V = np.ones((10, 1, 2), dtype=float)
         out = sliced_score_matching_loss(score_fn, analytic_obj)(X, V)
         ans = np.ones((10, 1), dtype=float) * 1226.5
-        self.assertAlmostEqual(np.linalg.norm(out - ans), 0., places=3)
+        self.assertAlmostEqual(np.linalg.norm(out - ans), 0.0, places=3)
 
     def test_train_step(self):
         # simple linear model that we can compute the gradients for by hand
         score_network = TestNetwork(2, 2)
 
-        # setting the PRNG with fixed seed means initialisation is consistent for testing
-        # using SGD
+        # setting the PRNG with fixed seed means initialisation is consistent for
+        # testing using SGD
         state = create_train_state(score_network, random.PRNGKey(0), 1e-3, 2, sgd)
 
         # Jax is row-based, so we have to work with the kernel transpose
-        W = state.params['Dense_0']['kernel'].T
-        b = state.params['Dense_0']['bias']
+        W = state.params["Dense_0"]["kernel"].T
+        b = state.params["Dense_0"]["bias"]
 
-        x = np.array([2., 7.])
+        x = np.array([2.0, 7.0])
         v = np.ones((1, 2), dtype=float)
         s = W @ x.T + b
 
@@ -128,30 +131,45 @@ class TestKernels(unittest.TestCase):
         state = sliced_score_matching_train_step(state, X, V, analytic_obj)
 
         # Jax is row based, so transpose W_
-        self.assertAlmostEqual(np.linalg.norm(W_.T - state.params["Dense_0"]["kernel"]), 0., places=3)
-        self.assertAlmostEqual(np.linalg.norm(b_ - state.params["Dense_0"]["bias"]), 0., places=3)
+        self.assertAlmostEqual(
+            np.linalg.norm(W_.T - state.params["Dense_0"]["kernel"]), 0.0, places=3
+        )
+        self.assertAlmostEqual(
+            np.linalg.norm(b_ - state.params["Dense_0"]["bias"]), 0.0, places=3
+        )
 
     def test_univariate_gaussian_score(self):
-        mu = 0.
-        std_dev = 1.
+        mu = 0.0
+        std_dev = 1.0
         N = 1000
         np.random.seed(0)
         samples = np.random.normal(mu, std_dev, size=(N, 1))
-        true_score = lambda x: -(x - mu) / std_dev**2
+
+        def true_score(x):
+            return -(x - mu) / std_dev**2
+
         x = np.linspace(-2, 2).reshape(-1, 1)
         y_t = true_score(x)
 
         # noise conditioning
-        learned_score = sliced_score_matching(samples, random.normal, use_analytic=True, epochs=20)
+        learned_score = sliced_score_matching(
+            samples, random.normal, use_analytic=True, epochs=20
+        )
         y_l = learned_score(x)
         mae = np.abs(y_t - y_l).mean()
-        self.assertLessEqual(mae, 1.)
+        self.assertLessEqual(mae, 1.0)
 
         # no noise conditioning
-        learned_score = sliced_score_matching(samples, random.normal, noise_conditioning=False, use_analytic=True, epochs=20)
+        learned_score = sliced_score_matching(
+            samples,
+            random.normal,
+            noise_conditioning=False,
+            use_analytic=True,
+            epochs=20,
+        )
         y_l = learned_score(x)
         mae = np.abs(y_t - y_l).mean()
-        self.assertLessEqual(mae, 1.)
+        self.assertLessEqual(mae, 1.0)
 
     def test_multivariate_gaussian_score(self):
         pass
