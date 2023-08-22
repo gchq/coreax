6305ae39058e765a658436f1942cc08bed2455a3
tl82649 <tl82649>
2023-08-22 15:31:40 +0000

    score matching formatting updated with type annotations, black and 88 char wrap; references added to .bib. A few more unit tests remaining

--- examples/pounce_sm.py
+++ examples/pounce_sm.py
@@ -1,16 +1,14 @@
 # © Crown Copyright GCHQ
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+# file except in compliance with the License. You may obtain a copy of the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
 import numpy as np
 import matplotlib.pyplot as plt
 import imageio
@@ -32,8 +30,8 @@ os.makedirs(f"{dir}/coreset", exist_ok=True)
 Y_ = np.array(imageio.v2.mimread(f"{dir}/{fn}")[1:])
 Y = Y_.reshape(Y_.shape[0], -1)
 
-# run PCA to reduce the dimension of the images whilst minimising effects on some of the statistical
-# properties, i.e. variance.
+# run PCA to reduce the dimension of the images whilst minimising effects on some of the
+# statistical properties, i.e. variance.
 p = 25
 pca = PCA(p)
 X = pca.fit_transform(Y)
@@ -47,34 +45,37 @@ idx = np.random.choice(X.shape[0], N, replace=False)
 nu = median_heuristic(X[idx])
 
 # define the kernel
-k = lambda x, y : rbf_kernel(x, y, jnp.float32(nu)**2)/(nu * jnp.sqrt(2. * jnp.pi))
+k = lambda x, y: rbf_kernel(x, y, jnp.float32(nu) ** 2) / (nu * jnp.sqrt(2.0 * jnp.pi))
 weighted = True
 
 # learn a score function
-score_function = sliced_score_matching(X, rademacher, use_analytic=True, epochs=200)
+score_function = sliced_score_matching(
+    X, rademacher, use_analytic=True, epochs=100, L=100, sigma=1.0
+)
 
 # run Stein kernel herding in block mode to avoid GPU memory issues
-coreset, Kc, Kbar = \
-    stein_kernel_herding_block(X, C, stein_kernel_pc_imq_element, score_function, nu=nu, max_size=1000, sm=True)
+coreset, Kc, Kbar = stein_kernel_herding_block(
+    X, C, stein_kernel_pc_imq_element, score_function, nu=nu, max_size=1000, sm=True
+)
 
 # sort the coreset ready for producing the output video
 coreset = jnp.sort(coreset)
-print('Coreset:', coreset)
+print("Coreset:", coreset)
 
 # Save a new video. Y_ is the original sequence with dimensions preserved
 coreset_images = Y_[coreset]
 imageio.mimsave(f"{dir}/coreset/coreset_sm.gif", coreset_images)
 
-# plot to visualise which frames were chosen from the sequence
-# action frames are where the "pounce" occurs
+# plot to visualise which frames were chosen from the sequence action frames are where
+# the "pounce" occurs
 action_frames = np.arange(63, 85)
 x = np.arange(N)
 y = np.zeros(N)
-y[coreset] = 1.
+y[coreset] = 1.0
 z = np.zeros(N)
-z[jnp.intersect1d(coreset, action_frames)] = 1.
+z[jnp.intersect1d(coreset, action_frames)] = 1.0
 plt.figure(figsize=(20, 3))
-plt.bar(x, y, alpha=.5)
+plt.bar(x, y, alpha=0.5)
 plt.bar(x, z)
 plt.xlabel("Frame")
 plt.ylabel("Chosen")
