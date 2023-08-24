729a3468021377ed6554b78dd6694f0686560b6c
tl82649 <tl82649>
2023-08-24 07:35:27 +0000

    moved create train state code into networks

--- coreax/networks.py
+++ coreax/networks.py
@@ -13,7 +13,12 @@
 # limitations under the License.
 
 from flax import linen as nn
+from flax.linen import Module
+from flax.training.train_state import TrainState
+from jax.random import PRNGKey
 from jax.typing import ArrayLike
+from jax import numpy as jnp
+from typing import Callable
 
 
 class ScoreNetwork(nn.Module):
@@ -32,3 +37,24 @@ class ScoreNetwork(nn.Module):
         x = nn.softplus(x)
         x = nn.Dense(self.output_dim)(x)
         return x
+
+
+def create_train_state(
+    module: Module,
+    rng: PRNGKey,
+    learning_rate: float,
+    dimension: int,
+    optimiser: Callable,
+) -> TrainState:
+    """Creates a flax TrainState for learning with
+
+    :param module: flax network class that inherits flax.nn.Module
+    :param rng: random number generator
+    :param learning_rate: optimiser learning rate
+    :param dimension: data dimension
+    :param optimiser: optax optimiser, e.g. optax.adam
+    :return: TrainState object
+    """
+    params = module.init(rng, jnp.ones((1, dimension)))["params"]
+    tx = optimiser(learning_rate)
+    return TrainState.create(apply_fn=module.apply, params=params, tx=tx)
