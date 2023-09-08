5ce14478116b4a015b94349093d40c16787e6847
tp832944 <139139729+tp832944@users.noreply.github.com>
2023-09-08 16:39:02 +0100

    chore: apply pre-commit hooks to all files
    Apply isort and Black effectively for the first time.
    
    refs: #60, #83, #84

--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import unittest
-from scipy.stats import ortho_group, norm
+
 import numpy as np
+from scipy.stats import norm, ortho_group
 
 from coreax.kernel import *
 
@@ -30,13 +31,13 @@ class TestKernels(unittest.TestCase):
         m = ortho_group.rvs(dim=2)
         x = m[0]
         y = m[1]
-        d = jnp.linalg.norm(x - y)**2
+        d = jnp.linalg.norm(x - y) ** 2
         td = sq_dist(x, y)
         self.assertAlmostEqual(d, td, places=3)
         td = sq_dist(x, x)
-        self.assertAlmostEqual(0., td, places=3)
+        self.assertAlmostEqual(0.0, td, places=3)
         td = sq_dist(y, y)
-        self.assertAlmostEqual(0., td, places=3)
+        self.assertAlmostEqual(0.0, td, places=3)
 
     def test_sq_dist_pairwise(self) -> None:
         """
@@ -46,11 +47,11 @@ class TestKernels(unittest.TestCase):
         d = 3
         m = ortho_group.rvs(dim=d)
         tinner = sq_dist_pairwise(m, m)
-        ans = np.ones((d, d)) * 2.
-        np.fill_diagonal(ans, 0.)
+        ans = np.ones((d, d)) * 2.0
+        np.fill_diagonal(ans, 0.0)
         # Frobenius norm
         td = jnp.linalg.norm(tinner - ans)
-        self.assertAlmostEqual(td, 0., places=3)
+        self.assertAlmostEqual(td, 0.0, places=3)
 
     def test_rbf_kernel(self) -> None:
         """
@@ -58,12 +59,12 @@ class TestKernels(unittest.TestCase):
 
         Note that the bandwidth is the 'variance' of the sq exp.
         """
-        bandwidth = np.float32(np.pi) / 2.
+        bandwidth = np.float32(np.pi) / 2.0
         x = np.arange(10)
-        y = x + 1.
-        ans = np.exp(-np.linalg.norm(x - y)**2 / (2. * bandwidth))
+        y = x + 1.0
+        ans = np.exp(-np.linalg.norm(x - y) ** 2 / (2.0 * bandwidth))
         tst = rbf_kernel(x, y, bandwidth)
-        self.assertAlmostEqual(jnp.linalg.norm(ans - tst), 0., places=3)
+        self.assertAlmostEqual(jnp.linalg.norm(ans - tst), 0.0, places=3)
 
     def test_laplace_kernel(self) -> None:
         """
@@ -71,12 +72,12 @@ class TestKernels(unittest.TestCase):
 
         Note that in this case, the norm isn't squared.
         """
-        bandwidth = np.float32(np.pi) / 2.
+        bandwidth = np.float32(np.pi) / 2.0
         x = np.arange(10)
-        y = x + 1.
-        ans = np.exp(-np.linalg.norm(x - y) / (2. * bandwidth))
+        y = x + 1.0
+        ans = np.exp(-np.linalg.norm(x - y) / (2.0 * bandwidth))
         tst = laplace_kernel(x, y, bandwidth)
-        self.assertAlmostEqual(jnp.linalg.norm(ans - tst), 0., places=3)
+        self.assertAlmostEqual(jnp.linalg.norm(ans - tst), 0.0, places=3)
 
     def test_pdiff(self) -> None:
         """
@@ -97,7 +98,7 @@ class TestKernels(unittest.TestCase):
             Z.append(list(row))
         Z = np.array(Z)
         tst = pdiff(X, Y)
-        self.assertAlmostEqual(jnp.linalg.norm(tst - Z), 0., places=3)
+        self.assertAlmostEqual(jnp.linalg.norm(tst - Z), 0.0, places=3)
 
     def test_gaussian_kernel(self) -> None:
         """
@@ -106,13 +107,13 @@ class TestKernels(unittest.TestCase):
         std_dev = np.e
         n = 10
         X = np.arange(n)
-        Y = X + 1.
+        Y = X + 1.0
         K = np.zeros((n, n))
         for i, x in enumerate(X):
             for j, y in enumerate(Y):
                 K[i, j] = norm(y, std_dev).pdf(x)
         tst = normalised_rbf(X, Y, std_dev)
-        self.assertAlmostEqual(jnp.linalg.norm(K - tst), 0., places=3)
+        self.assertAlmostEqual(jnp.linalg.norm(K - tst), 0.0, places=3)
 
     def test_pc_imq(self) -> None:
         """
@@ -123,13 +124,13 @@ class TestKernels(unittest.TestCase):
         std_dev = np.e
         n = 10
         X = np.arange(n)
-        Y = X + 1.
+        Y = X + 1.0
         K = np.zeros((n, n))
         for i, x in enumerate(X):
             for j, y in enumerate(Y):
-                K[i, j] = 1. / np.sqrt(1. + ((x - y)/std_dev)**2 / 2.)
-        tst = pc_imq (X, Y, std_dev)
-        self.assertAlmostEqual(jnp.linalg.norm(K - tst), 0., places=3)
+                K[i, j] = 1.0 / np.sqrt(1.0 + ((x - y) / std_dev) ** 2 / 2.0)
+        tst = pc_imq(X, Y, std_dev)
+        self.assertAlmostEqual(jnp.linalg.norm(K - tst), 0.0, places=3)
 
 
 if __name__ == "__main__":
