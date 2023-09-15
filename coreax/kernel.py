e88dd268da3ada014f778213b0e73a9e078cc621
tl82649 <tl82649>
2023-09-15 15:01:59 +0000

    added more tests for kernel.py; removed redundant functions from kernel.py; fixed some minor bugs in kernel.py; updated some docstrings in kernel.py; fixed some docstring typos

--- coreax/kernel.py
+++ coreax/kernel.py
@@ -51,7 +51,7 @@ def sq_dist_pairwise(x: ArrayLike, y: ArrayLike) -> Array:
 
 
 @jit
-def rbf_kernel(x: ArrayLike, y: ArrayLike, variance: float = 1.) -> Array:
+def rbf_kernel(x: ArrayLike, y: ArrayLike, variance: float = 1.0) -> Array:
     r"""
     Calculate the radial basis function (RBF) kernel for a pair of vectors.
 
@@ -64,7 +64,7 @@ def rbf_kernel(x: ArrayLike, y: ArrayLike, variance: float = 1.) -> Array:
 
 
 @jit
-def laplace_kernel(x: ArrayLike, y: ArrayLike, variance: float = 1.) -> Array:
+def laplace_kernel(x: ArrayLike, y: ArrayLike, variance: float = 1.0) -> Array:
     r"""
     Calculate the Laplace kernel for a pair of vectors.
 
@@ -105,8 +105,9 @@ def pdiff(x_array: ArrayLike, y_array: ArrayLike) -> Array:
 
 
 @jit
-def normalised_rbf(x_array: ArrayLike, y_array: ArrayLike, bandwidth: float = 1.) -> \
-        Array:
+def normalised_rbf(
+    x_array: ArrayLike, y_array: ArrayLike, bandwidth: float = 1.0
+) -> Array:
     r"""
     Evaluate the normalised Gaussian kernel pairwise.
 
@@ -116,13 +117,13 @@ def normalised_rbf(x_array: ArrayLike, y_array: ArrayLike, bandwidth: float = 1.
     :return: Pairwise kernel evaluations
     """
     square_distances = sq_dist_pairwise(x_array, y_array)
-    kernel = jnp.exp(-.5 * square_distances / bandwidth ** 2) / jnp.sqrt(2 * jnp.pi)
+    kernel = jnp.exp(-0.5 * square_distances / bandwidth**2) / jnp.sqrt(2 * jnp.pi)
 
     return kernel / bandwidth
 
 
 @jit
-def pc_imq(x_array: ArrayLike, y_array: ArrayLike, bandwidth: float = 1.) -> Array:
+def pc_imq(x_array: ArrayLike, y_array: ArrayLike, bandwidth: float = 1.0) -> Array:
     r"""
     Evaluate the pre-conditioned inverse multi-quadric kernel pairwise.
 
@@ -131,7 +132,7 @@ def pc_imq(x_array: ArrayLike, y_array: ArrayLike, bandwidth: float = 1.) -> Arr
     :param bandwidth: Kernel bandwidth (standard deviation). Optional, defaults to 1
     :return: Pairwise kernel evaluations
     """
-    scaling = 2 * bandwidth ** 2
+    scaling = 2 * bandwidth**2
     mq_array = sq_dist_pairwise(x_array, y_array) / scaling
     kernel = 1 / jnp.sqrt(1 + mq_array)
 
@@ -140,111 +141,113 @@ def pc_imq(x_array: ArrayLike, y_array: ArrayLike, bandwidth: float = 1.) -> Arr
 
 @jit
 def grad_rbf_y(
-        x_array: ArrayLike,
-        y_array: ArrayLike,
-        bandwidth: float = 1.,
-        gram_matrix: ArrayLike | None = None,
+    x_array: ArrayLike,
+    y_array: ArrayLike,
+    bandwidth: float = 1.0,
+    gram_matrix: ArrayLike | None = None,
 ) -> Array:
     r"""
-    Calculate the gradient of the normalised radial basis function w.r.t. y_array.
+    Calculate the *element-wise* gradient of the normalised radial basis function w.r.t.
+    y_array.
 
     :param x_array: First set of vectors as a :math:`n \times d` array
     :param y_array: Second set of vectors as a :math:`m \times d` array
     :param bandwidth: Kernel bandwidth (standard deviation). Optional, defaults to 1
-    :param gram_matrix: Gram matrix. Optional, if :data:`None` or omitted, defaults to
-        a normalised Gaussian kernel
-    :return: Gradients at each `x_array X y_array` point, an
-        :math:`m \times n \times d` array
+    :param gram_matrix: Gram matrix. Optional, if :data:`None` or omitted, defaults to a
+        normalised Gaussian kernel
+    :return: Gradients at each `x_array X y_array` point, an :math:`m \times n \times d`
+        array
     """
     if gram_matrix is None:
-        gram_matrix = normalised_rbf(y_array, x_array, bandwidth=bandwidth)
+        gram_matrix = normalised_rbf(x_array, y_array, bandwidth=bandwidth)
     else:
         gram_matrix = jnp.asarray(gram_matrix)
 
-    distances = pdiff(y_array, x_array)
+    distances = pdiff(x_array, y_array)
 
-    return distances * gram_matrix[:, :, None] / bandwidth
+    return distances * gram_matrix[:, :, None] / bandwidth**2
 
 
 @jit
 def grad_rbf_x(
-        x_array: ArrayLike,
-        y_array: ArrayLike,
-        bandwidth: float = 1.,
-        gram_matrix: ArrayLike | None = None,
+    x_array: ArrayLike,
+    y_array: ArrayLike,
+    bandwidth: float = 1.0,
+    gram_matrix: ArrayLike | None = None,
 ) -> Array:
     r"""
-    Calculate the gradient of the normalised radial basis function w.r.t. x_array.
+    Calculate the *element-wise* gradient of the normalised radial basis function w.r.t.
+    x_array.
 
     :param x_array: First set of vectors as a :math:`n \times d` array
     :param y_array: Second set of vectors as a :math:`m \times d` array
     :param bandwidth: Kernel bandwidth (standard deviation). Optional, defaults to 1
-    :param gram_matrix: Gram matrix. Optional, if :data:`None` or omitted, defaults to
-        a normalised Gaussian kernel
-    :return: Gradients at each `x_array X y_array` point, an
-        :math:`n \times m \times d` array
+    :param gram_matrix: Gram matrix. Optional, if :data:`None` or omitted, defaults to a
+        normalised Gaussian kernel
+    :return: Gradients at each `x_array X y_array` point, an :math:`m \times n \times d`
+        array
     """
-    return -jnp.transpose(grad_rbf_y(x_array, y_array, bandwidth, gram_matrix),
-                          (1, 0, 2))
+    return -grad_rbf_y(x_array, y_array, bandwidth, gram_matrix)
 
 
 @jit
 def grad_pc_imq_y(
-        x_array: ArrayLike,
-        y_array: ArrayLike,
-        bandwidth: float = 1.,
-        gram_matrix: ArrayLike | None = None,
+    x_array: ArrayLike,
+    y_array: ArrayLike,
+    bandwidth: float = 1.0,
+    gram_matrix: ArrayLike | None = None,
 ) -> Array:
     r"""
-    Calculate gradient of the pre-conditioned inverse multi-quadric w.r.t. y_array.
+    Calculate *element-wise* gradient of the pre-conditioned inverse multi-quadric
+    w.r.t. y_array.
 
     :param x_array: First set of vectors as a :math:`n \times d` array
     :param y_array: Second set of vectors as a :math:`m \times d` array
     :param bandwidth: Kernel bandwidth (standard deviation). Optional, defaults to 1
-    :param gram_matrix: Gram matrix. Optional, if :data:`None` or omitted, defaults to
-        a preconditioned inverse multi-quadric kernel
-    :return: Gradients at each `x_array X y_array` point, an
-        :math:`m \times n \times d` array
+    :param gram_matrix: Gram matrix. Optional, if :data:`None` or omitted, defaults to a
+        preconditioned inverse multi-quadric kernel
+    :return: Gradients at each `x_array X y_array` point, an :math:`m \times n \times d`
+        array
     """
     scaling = 2 * bandwidth**2
     if gram_matrix is None:
-        gram_matrix = pc_imq(y_array, x_array, bandwidth)
+        gram_matrix = pc_imq(x_array, y_array, bandwidth)
     else:
         gram_matrix = jnp.asarray(gram_matrix)
-    mq_array = pdiff(y_array, x_array)
+    mq_array = pdiff(x_array, y_array)
 
-    return gram_matrix[:, :, None]**3 * mq_array / scaling
+    return gram_matrix[:, :, None] ** 3 * mq_array / scaling
 
 
 @jit
 def grad_pc_imq_x(
-        x_array: ArrayLike,
-        y_array: ArrayLike,
-        bandwidth: float = 1.,
-        gram_matrix: ArrayLike | None = None,
+    x_array: ArrayLike,
+    y_array: ArrayLike,
+    bandwidth: float = 1.0,
+    gram_matrix: ArrayLike | None = None,
 ) -> Array:
     r"""
-    Calculate gradient of the pre-conditioned inverse multi-quadric w.r.t. x_array.
+    Calculate *element-wise* gradient of the pre-conditioned inverse multi-quadric
+    w.r.t. x_array.
 
     :param x_array: First set of vectors as a :math:`n \times d` array
     :param y_array: Second set of vectors as a :math:`m \times d` array
     :param bandwidth: Kernel bandwidth (standard deviation). Optional, defaults to 1
-    :param gram_matrix: Gram matrix. Optional, if :data:`None` or omitted, defaults to
-        a preconditioned inverse multi-quadric kernel
-    :return: Gradients at each `x_array X y_array` point, an
-        :math:`n \times m \times d` array
+    :param gram_matrix: Gram matrix. Optional, if :data:`None` or omitted, defaults to a
+        preconditioned inverse multi-quadric kernel
+    :return: Gradients at each `x_array X y_array` point, an :math:`m \times n \times d`
+        array
     """
-    return -jnp.transpose(grad_pc_imq_y(x_array, y_array, bandwidth, gram_matrix),
-                          (1, 0, 2))
+    return -grad_pc_imq_y(x_array, y_array, bandwidth, gram_matrix)
 
 
 @jit
 def rbf_div_x_grad_y(
-        x_array: ArrayLike,
-        y_array: ArrayLike,
-        bandwidth: float = 1.,
-        num_data_points: int | None = None,
-        gram_matrix: ArrayLike | None = None,
+    x_array: ArrayLike,
+    y_array: ArrayLike,
+    bandwidth: float = 1.0,
+    num_data_points: int | None = None,
+    gram_matrix: ArrayLike | None = None,
 ) -> Array:
     r"""
     Apply divergence operator on gradient of RBF kernel with respect to `y_array`.
@@ -267,17 +270,20 @@ def rbf_div_x_grad_y(
     if num_data_points is None:
         num_data_points = x_array.shape[0]
 
-    return gram_matrix / bandwidth * \
-        (num_data_points - sq_dist_pairwise(x_array, y_array) / bandwidth)
+    return (
+        gram_matrix
+        / bandwidth
+        * (num_data_points - sq_dist_pairwise(x_array, y_array) / bandwidth)
+    )
 
 
 @jit
 def pc_imq_div_x_grad_y(
-        x_array: ArrayLike,
-        y_array: ArrayLike,
-        bandwidth: float = 1.,
-        num_data_points: int | None = None,
-        gram_matrix: ArrayLike | None = None,
+    x_array: ArrayLike,
+    y_array: ArrayLike,
+    bandwidth: float = 1.0,
+    num_data_points: int | None = None,
+    gram_matrix: ArrayLike | None = None,
 ) -> Array:
     r"""
     Apply divergence operator on gradient of PC-IMQ kernel with respect to y_array.
@@ -294,15 +300,16 @@ def pc_imq_div_x_grad_y(
         a normalised Gaussian kernel
     :return: Divergence operator, an :math:`n \times m` matrix
     """
-    scaling = 2 * bandwidth ** 2
+    scaling = 2 * bandwidth**2
     x_array = jnp.asarray(x_array)
     if gram_matrix is None:
         gram_matrix = pc_imq(x_array, y_array, bandwidth=bandwidth)
     if num_data_points is None:
         num_data_points = x_array.shape[0]
-    return num_data_points / \
-        scaling * gram_matrix ** 3 - 3 * sq_dist_pairwise(x_array, y_array) / \
-        scaling ** 2 * gram_matrix ** 5
+    return (
+        num_data_points / scaling * gram_matrix**3
+        - 3 * sq_dist_pairwise(x_array, y_array) / scaling**2 * gram_matrix**5
+    )
 
 
 @jit
@@ -317,15 +324,17 @@ def median_heuristic(x_array: ArrayLike) -> Array:
     # calculate square distances as an upper triangular matrix
     square_distances = jnp.triu(sq_dist_pairwise(x_array, x_array), k=1)
     # calculate the median
-    median_square_distance = \
-        jnp.median(square_distances[jnp.triu_indices_from(square_distances, k=1)])
-    
-    return jnp.sqrt(median_square_distance / 2.)
+    median_square_distance = jnp.median(
+        square_distances[jnp.triu_indices_from(square_distances, k=1)]
+    )
+
+    return jnp.sqrt(median_square_distance / 2.0)
 
 
 @jit
-def rbf_f_x(random_var_values: ArrayLike, kde_data: ArrayLike, bandwidth: float) -> \
-        tuple[Array, Array]:
+def rbf_f_x(
+    random_var_values: ArrayLike, kde_data: ArrayLike, bandwidth: float
+) -> tuple[Array, Array]:
     r"""
     Construct PDF of `random_var_values` by kernel density estimation for a radial
     basis function.
@@ -338,17 +347,17 @@ def rbf_f_x(random_var_values: ArrayLike, kde_data: ArrayLike, bandwidth: float)
     """
     kernel = normalised_rbf(random_var_values, kde_data, bandwidth)
     kernel_mean = kernel.mean(axis=1)
-    
+
     return kernel_mean, kernel
 
 
 @jit
 def rbf_grad_log_f_x(
-        random_var_values: ArrayLike,
-        kde_data: ArrayLike,
-        bandwidth: float,
-        gram_matrix: ArrayLike | None = None,
-        kernel_mean: ArrayLike | None = None,
+    random_var_values: ArrayLike,
+    kde_data: ArrayLike,
+    bandwidth: float,
+    gram_matrix: ArrayLike | None = None,
+    kernel_mean: ArrayLike | None = None,
 ) -> Array:
     r"""
     Compute gradient of log-PDF of `random_var_values`.
@@ -373,63 +382,17 @@ def rbf_grad_log_f_x(
         kernel_mean, gram_matrix = rbf_f_x(random_var_values, kde_data, bandwidth)
     else:
         kernel_mean = jnp.asarray(kernel_mean)
-    num_kde_points = kde_data.shape[0]
-    gradients = \
-        grad_rbf_x(random_var_values, kde_data, bandwidth, gram_matrix).mean(axis=1)
-    
-    return gradients / (num_kde_points * kernel_mean[:, None])
+    gradients = grad_rbf_x(random_var_values, kde_data, bandwidth, gram_matrix).mean(
+        axis=1
+    )
+
+    return gradients / kernel_mean[:, None]
 
 
 @jit
-def grad_rbf_x(
-        x_array: ArrayLike,
-        y_array: ArrayLike,
-        bandwidth: float,
-        kernel: ArrayLike | None = None,
+def stein_kernel_rbf(
+    x_array: ArrayLike, y_array: ArrayLike, bandwidth: float = 1.0
 ) -> Array:
-    r"""
-    Compute gradient of the radial basis function kernel with respect to `x_array`.
-
-    :param x_array: First set of vectors as a :math:`n \times d` array
-    :param y_array: Second set of vectors as a :math:`m \times d` array
-    :param bandwidth: Kernel bandwidth (standard deviation)
-    :param kernel: Gram matrix, an :math:`n \times m` array. Optional, if :data:`None`
-        or omitted, defaults to a normalised Gaussian kernel
-    :return: Gradient evaluated at values of `x_array`, as a :math:`n \times d` array
-    """
-    if kernel is None:
-        kernel = normalised_rbf(x_array, y_array, bandwidth)
-    else:
-        kernel = jnp.asarray(kernel)
-
-    scaled_distances = -pdiff(x_array, y_array) / bandwidth
-    return scaled_distances * kernel[:, :, None]
-
-
-@jit
-def grad_rbf_y(
-        x_array: ArrayLike,
-        y_array: ArrayLike,
-        bandwidth: float,
-        gram_matrix: ArrayLike | None = None,
-) -> Array:
-    r"""
-    Compute gradient of the radial basis function kernel with respect to `y_array`.
-
-    :param x_array: First set of vectors as a :math:`n \times d` array
-    :param y_array: Second set of vectors as a :math:`n \times d` array
-    :param bandwidth: Kernel bandwidth (standard deviation)
-    :param gram_matrix: Gram matrix, an :math:`n \times m` array. Optional, if
-        :data:`None` or omitted, defaults to a normalised Gaussian kernel
-    :return: Gradient evaluated at values of `y_array`, as a :math:`m \times d` array
-    """
-    return -jnp.transpose(grad_rbf_x(x_array, y_array, bandwidth, gram_matrix),
-                          (1, 0, 2))
-
-
-@jit
-def stein_kernel_rbf(x_array: ArrayLike, y_array: ArrayLike, bandwidth: float = 1.) -> \
-        Array:
     r"""
     Compute a kernel from an RBF kernel with the canonical Stein operator.
 
@@ -449,7 +412,9 @@ def stein_kernel_rbf(x_array: ArrayLike, y_array: ArrayLike, bandwidth: float = 
     # n x m x d
     grad_k_x = grad_rbf_x(x_array, y_array, bandwidth, rbf_kernel_)
     # m x n x d
-    grad_k_y = grad_rbf_y(x_array, y_array, bandwidth, rbf_kernel_)
+    grad_k_y = jnp.transpose(
+        grad_rbf_y(x_array, y_array, bandwidth, rbf_kernel_), (1, 0, 2)
+    )
     # n x d
     grad_log_p_x = rbf_grad_log_f_x(x_array, y_array, bandwidth)
     # m x d
@@ -468,8 +433,9 @@ def stein_kernel_rbf(x_array: ArrayLike, y_array: ArrayLike, bandwidth: float = 
 
 
 @jit
-def stein_kernel_pc_imq(x_array: ArrayLike, y_array: ArrayLike, bandwidth: float = 1.) \
-        -> Array:
+def stein_kernel_pc_imq(
+    x_array: ArrayLike, y_array: ArrayLike, bandwidth: float = 1.0
+) -> Array:
     r"""
     Compute a kernel from a PC-IMQ kernel with the canonical Stein operator.
 
@@ -492,7 +458,9 @@ def stein_kernel_pc_imq(x_array: ArrayLike, y_array: ArrayLike, bandwidth: float
     # n x m x d
     grad_k_x = grad_pc_imq_x(x_array, y_array, bandwidth, pc_imq_kernel)
     # m x n x d
-    grad_k_y = grad_pc_imq_y(x_array, y_array, bandwidth, pc_imq_kernel)
+    grad_k_y = jnp.transpose(
+        grad_pc_imq_y(x_array, y_array, bandwidth, pc_imq_kernel), (1, 0, 2)
+    )
     # n x d
     grad_log_p_x = rbf_grad_log_f_x(x_array, y_array, bandwidth)
     # m x d
@@ -512,12 +480,12 @@ def stein_kernel_pc_imq(x_array: ArrayLike, y_array: ArrayLike, bandwidth: float
 
 @jit
 def stein_kernel_pc_imq_element(
-        x: ArrayLike,
-        y: ArrayLike,
-        grad_log_p_x: ArrayLike,
-        grad_log_p_y: ArrayLike,
-        num_data_points: int,
-        bandwidth: float = 1.,
+    x: ArrayLike,
+    y: ArrayLike,
+    grad_log_p_x: ArrayLike,
+    grad_log_p_y: ArrayLike,
+    dimension: int,
+    bandwidth: float = 1.0,
 ) -> Array:
     r"""
     Evaluate the kernel element at `(x,y)`.
@@ -531,7 +499,7 @@ def stein_kernel_pc_imq_element(
         array
     :param grad_log_p_y: Gradient of log-PDF evaluated at `y`, a :math:`1 \times d`
         array
-    :param num_data_points: Number of data points in the generating set
+    :param dimension: Dimension of the input data.
     :param bandwidth: Kernel bandwidth (standard deviation). Optional, defaults to 1
     :return: Kernel evaluation at `(x,y)`, 0-dimensional array
     """
@@ -542,8 +510,9 @@ def stein_kernel_pc_imq_element(
     # n x m
     pc_imq_kernel = pc_imq(x, y, bandwidth)
     # n x m
-    divergence: Array = \
-        pc_imq_div_x_grad_y(x, y, bandwidth, num_data_points, pc_imq_kernel)
+    divergence: Array = pc_imq_div_x_grad_y(
+        x, y, bandwidth, dimension, pc_imq_kernel
+    )
     # n x m x d
     grad_p_x = jnp.squeeze(grad_pc_imq_x(x, y, bandwidth, pc_imq_kernel))
     # m x n x d
--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -13,6 +13,9 @@
 # limitations under the License.
 import unittest
 from scipy.stats import ortho_group, norm
+from jax import grad, vjp
+from typing import Callable
+from functools import partial
 import numpy as np
 
 from coreax.kernel import *
@@ -30,13 +33,13 @@ class TestKernels(unittest.TestCase):
         m = ortho_group.rvs(dim=2)
         x = m[0]
         y = m[1]
-        d = jnp.linalg.norm(x - y)**2
+        d = jnp.linalg.norm(x - y) ** 2
         td = sq_dist(x, y)
         self.assertAlmostEqual(d, td, places=3)
         td = sq_dist(x, x)
-        self.assertAlmostEqual(0., td, places=3)
+        self.assertAlmostEqual(0.0, td, places=3)
         td = sq_dist(y, y)
-        self.assertAlmostEqual(0., td, places=3)
+        self.assertAlmostEqual(0.0, td, places=3)
 
     def test_sq_dist_pairwise(self) -> None:
         """
@@ -46,11 +49,11 @@ class TestKernels(unittest.TestCase):
         d = 3
         m = ortho_group.rvs(dim=d)
         tinner = sq_dist_pairwise(m, m)
-        ans = np.ones((d, d)) * 2.
-        np.fill_diagonal(ans, 0.)
+        ans = np.ones((d, d)) * 2.0
+        np.fill_diagonal(ans, 0.0)
         # Frobenius norm
         td = jnp.linalg.norm(tinner - ans)
-        self.assertAlmostEqual(td, 0., places=3)
+        self.assertAlmostEqual(td, 0.0, places=3)
 
     def test_rbf_kernel(self) -> None:
         """
@@ -58,12 +61,12 @@ class TestKernels(unittest.TestCase):
 
         Note that the bandwidth is the 'variance' of the sq exp.
         """
-        bandwidth = np.float32(np.pi) / 2.
+        bandwidth = np.float32(np.pi) / 2.0
         x = np.arange(10)
-        y = x + 1.
-        ans = np.exp(-np.linalg.norm(x - y)**2 / (2. * bandwidth))
+        y = x + 1.0
+        ans = np.exp(-np.linalg.norm(x - y) ** 2 / (2.0 * bandwidth))
         tst = rbf_kernel(x, y, bandwidth)
-        self.assertAlmostEqual(jnp.linalg.norm(ans - tst), 0., places=3)
+        self.assertAlmostEqual(jnp.linalg.norm(ans - tst), 0.0, places=3)
 
     def test_laplace_kernel(self) -> None:
         """
@@ -71,12 +74,12 @@ class TestKernels(unittest.TestCase):
 
         Note that in this case, the norm isn't squared.
         """
-        bandwidth = np.float32(np.pi) / 2.
+        bandwidth = np.float32(np.pi) / 2.0
         x = np.arange(10)
-        y = x + 1.
-        ans = np.exp(-np.linalg.norm(x - y) / (2. * bandwidth))
+        y = x + 1.0
+        ans = np.exp(-np.linalg.norm(x - y) / (2.0 * bandwidth))
         tst = laplace_kernel(x, y, bandwidth)
-        self.assertAlmostEqual(jnp.linalg.norm(ans - tst), 0., places=3)
+        self.assertAlmostEqual(jnp.linalg.norm(ans - tst), 0.0, places=3)
 
     def test_pdiff(self) -> None:
         """
@@ -97,7 +100,7 @@ class TestKernels(unittest.TestCase):
             Z.append(list(row))
         Z = np.array(Z)
         tst = pdiff(X, Y)
-        self.assertAlmostEqual(jnp.linalg.norm(tst - Z), 0., places=3)
+        self.assertAlmostEqual(jnp.linalg.norm(tst - Z), 0.0, places=3)
 
     def test_gaussian_kernel(self) -> None:
         """
@@ -106,13 +109,13 @@ class TestKernels(unittest.TestCase):
         std_dev = np.e
         n = 10
         X = np.arange(n)
-        Y = X + 1.
+        Y = X + 1.0
         K = np.zeros((n, n))
         for i, x in enumerate(X):
             for j, y in enumerate(Y):
                 K[i, j] = norm(y, std_dev).pdf(x)
         tst = normalised_rbf(X, Y, std_dev)
-        self.assertAlmostEqual(jnp.linalg.norm(K - tst), 0., places=3)
+        self.assertAlmostEqual(jnp.linalg.norm(K - tst), 0.0, places=3)
 
     def test_pc_imq(self) -> None:
         """
@@ -123,13 +126,161 @@ class TestKernels(unittest.TestCase):
         std_dev = np.e
         n = 10
         X = np.arange(n)
-        Y = X + 1.
+        Y = X + 1.0
         K = np.zeros((n, n))
         for i, x in enumerate(X):
             for j, y in enumerate(Y):
-                K[i, j] = 1. / np.sqrt(1. + ((x - y)/std_dev)**2 / 2.)
-        tst = pc_imq (X, Y, std_dev)
-        self.assertAlmostEqual(jnp.linalg.norm(K - tst), 0., places=3)
+                K[i, j] = 1.0 / np.sqrt(1.0 + ((x - y) / std_dev) ** 2 / 2.0)
+        tst = pc_imq(X, Y, std_dev)
+        self.assertAlmostEqual(jnp.linalg.norm(K - tst), 0.0, places=3)
+
+    def test_rbf_f_x(self) -> None:
+        r"""
+        Test the kernel density estimation (KDE) PDF for a radial basis function.
+        """
+        std_dev = np.e
+        n = 10
+        X = np.arange(n)
+        Y = X + 1.0
+        K = np.zeros((n, n))
+        for i, x in enumerate(X):
+            for j, y in enumerate(Y):
+                K[i, j] = norm(y, std_dev).pdf(x)
+        tst_mean, tst_val = rbf_f_x(X, Y, std_dev)
+        self.assertAlmostEqual(
+            jnp.linalg.norm(K.mean(axis=1) - tst_mean), 0.0, places=3
+        )
+        self.assertAlmostEqual(jnp.linalg.norm(K - tst_val), 0.0, places=3)
+
+    def test_grad_rbf_x(self) -> None:
+        r"""
+        Test the gradient of the RBF kernel (analytic).
+        """
+        bandwidth = 1 / np.sqrt(2)
+        n = 10
+        d = 2
+        X = np.random.random((n, d))
+        Y = np.random.random((n, d))
+        K = np.zeros((n, n, d))
+        for i, x in enumerate(X):
+            for j, y in enumerate(Y):
+                K[i, j] = (
+                    -(x - y)
+                    / bandwidth**3
+                    * np.exp(-np.linalg.norm(x - y) ** 2 / (2 * bandwidth**2))
+                    / (np.sqrt(2 * np.pi))
+                )
+
+        tst = grad_rbf_x(X, Y, bandwidth)
+        self.assertAlmostEqual(jnp.linalg.norm(K - tst), 0.0, places=3)
+
+    def test_grad_rbf_y(self) -> None:
+        r"""
+        Test the gradient of the RBF kernel (analytic).
+        """
+        bandwidth = 1 / np.sqrt(2)
+        n = 10
+        d = 2
+        X = np.random.random((n, d))
+        Y = np.random.random((n, d))
+        K = np.zeros((n, n, d))
+        for i, x in enumerate(X):
+            for j, y in enumerate(Y):
+                K[i, j] = (
+                    (x - y)
+                    / bandwidth**3
+                    * np.exp(-np.linalg.norm(x - y) ** 2 / (2 * bandwidth**2))
+                    / (np.sqrt(2 * np.pi))
+                )
+
+        tst = grad_rbf_y(X, Y, bandwidth)
+        self.assertAlmostEqual(jnp.linalg.norm(K - tst), 0.0, places=3)
+
+    def test_grad_pc_imq_x(self) -> None:
+        r"""
+        Test the gradient of the PC-IMQ kernel wrt x argument
+        """
+        bandwidth = 1 / np.sqrt(2)
+        n = 10
+        d = 2
+        X = np.random.random((n, d))
+        Y = np.random.random((n, d))
+        K = np.zeros((n, n, d))
+        for i, x in enumerate(X):
+            for j, y in enumerate(Y):
+                K[i, j] = -(x - y) / (1 + np.linalg.norm(x - y) ** 2) ** (3 / 2)
+        tst = grad_pc_imq_x(X, Y, bandwidth)
+        self.assertAlmostEqual(jnp.linalg.norm(K - tst), 0.0, places=3)
+
+    def test_grad_pc_imq_y(self) -> None:
+        r"""
+        Test the gradient of the PC-IMQ kernel wrt x argument
+        """
+        bandwidth = 1 / np.sqrt(2)
+        n = 10
+        d = 2
+        X = np.random.random((n, d))
+        Y = np.random.random((n, d))
+        K = np.zeros((n, n, d))
+        for i, x in enumerate(X):
+            for j, y in enumerate(Y):
+                K[i, j] = (x - y) / (1 + np.linalg.norm(x - y) ** 2) ** (3 / 2)
+        tst = grad_pc_imq_y(X, Y, bandwidth)
+        self.assertAlmostEqual(jnp.linalg.norm(K - tst), 0.0, places=3)
+
+    def test_rbf_grad_log_f_x(self) -> None:
+        r"""
+        Test the score function of an RBF
+        """
+        bandwidth = 1 / np.sqrt(2)
+        n = 10
+        d = 2
+        X = np.random.random((n, d))
+        kde_points = np.random.random((n, d))
+        kde = lambda x: (
+            np.exp(-np.linalg.norm(x - kde_points, axis=1)[:, None] ** 2
+                / (2 * bandwidth**2)
+            )
+            / (np.sqrt(2 * np.pi) * bandwidth)
+        ).mean(axis=0)
+        J = np.zeros((n, d))
+        for i, x in enumerate(X):
+            J[i] = (
+                -(x - kde_points)
+                / bandwidth**3
+                * np.exp(
+                    -np.linalg.norm(x - kde_points, axis=1)[:, None] ** 2
+                    / (2 * bandwidth**2)
+                )
+                / (np.sqrt(2 * np.pi))
+            ).mean(axis=0) / (kde(x)[:, None])
+        tst = rbf_grad_log_f_x(X, kde_points, bandwidth)
+        self.assertAlmostEqual(jnp.linalg.norm(J - tst), 0.0, places=3)
+
+    def test_stein_kernel_pc_imq_element(self) -> None:
+        """
+        Test the Stein kernel with PC-IMQ base and score fn -x
+        """
+        n = 10
+        d = 2
+        bandwidth = 1 / np.sqrt(2)
+        score_fn = lambda x: -x
+        beta = .5
+        def k_x_y(x, y):
+            norm_sq = np.linalg.norm(x - y)**2
+            l = -3 * norm_sq / (1 + norm_sq)**2.5
+            m = 2 * beta * (d + np.dot(score_fn(x) - score_fn(y), x - y)) / (1 + norm_sq)**1.5
+            r = np.dot(score_fn(x), score_fn(y)) / (1 + norm_sq)**.5
+            return l + m + r
+        X = np.random.random((n, d))
+        Y = np.random.random((n, d))
+        K = np.zeros((n, n))
+        K_ans = np.zeros((n, n))
+        for i, x in enumerate(X):
+            for j, y in enumerate(Y):
+                K_ans[i, j] = k_x_y(x, y)
+                K[i, j] = stein_kernel_pc_imq_element(x, y, score_fn(x), score_fn(y), d, bandwidth)
+        self.assertAlmostEqual(jnp.linalg.norm(K - K_ans), 0., places=3)
 
 
 if __name__ == "__main__":
