79bd92b654f67a820ee41249baa396e7cf500153
bk958178 <144437611+bk958178@users.noreply.github.com>
2023-10-19 12:54:47 +0100

    feat: update refine tests

--- tests/unit/test_refine.py
+++ tests/unit/test_refine.py
@@ -17,12 +17,12 @@ import unittest
 import jax.numpy as jnp
 from jax import random, vmap
 
-import coreax.kernel as ck
 import coreax.refine as cr
+from coreax.kernel import rbf_kernel
 
 
 class TestMetrics(unittest.TestCase):
-    r"""
+    """
     Tests related to refine.py functions.
     """
 
@@ -33,90 +33,86 @@ class TestMetrics(unittest.TestCase):
         """
 
         x = jnp.asarray([[0, 0], [1, 1], [0, 0], [1, 1]])
-        n = len(x)
         S = jnp.asarray([0, 1])
 
         K = vmap(
-            vmap(ck.rbf_kernel, in_axes=(None, 0), out_axes=0),
+            vmap(rbf_kernel, in_axes=(None, 0), out_axes=0),
             in_axes=(0, None),
             out_axes=0,
         )(x, x)
-        K_mean = K.sum(axis=1) / n
+        K_mean = K.mean(axis=1)
 
-        refine_test = cr.refine(x, S, ck.rbf_kernel, K_mean)
+        refine_test = cr.refine(x, S, rbf_kernel, K_mean)
         self.assertCountEqual(refine_test, S)
 
     def test_refine_ints(self) -> None:
         r"""
         For a toy example, X = [[0,0], [1,1], [2,2]], the 2-point coreset that minimises
-        the MMD is specified y the indices S = [0, 2], ie X_c =  [[0,0], [[2,2]].
+        the MMD is specified by the indices S = [0, 2], ie X_c =  [[0,0], [[2,2]].
 
         Test this example, for a random 2-point coreset, that refine returns [0, 2].
         """
 
         x = jnp.asarray([[0, 0], [1, 1], [2, 2]])
-        n = len(x)
 
         key = random.PRNGKey(0)
-        S1 = random.randint(key, (), 0, 2)
-        S2 = random.randint(key, (), 0, 2)
+        S1 = random.randint(key, shape=(), minval=0, maxval=2)
+        S2 = random.randint(key, shape=(), minval=0, maxval=2)
         S = jnp.asarray([S1, S2])
 
         K = vmap(
-            vmap(ck.rbf_kernel, in_axes=(None, 0), out_axes=0),
+            vmap(rbf_kernel, in_axes=(None, 0), out_axes=0),
             in_axes=(0, None),
             out_axes=0,
         )(x, x)
-        K_mean = K.sum(axis=1) / n
+        K_mean = K.mean(axis=1)
 
-        refine_test = cr.refine(x, S, ck.rbf_kernel, K_mean)
+        refine_test = cr.refine(x, S, rbf_kernel, K_mean)
 
         self.assertCountEqual(refine_test, jnp.asarray([0, 2]))
 
     def test_refine_rand(self):
         r"""
         For a toy example, X = [[0,0], [1,1], [2,2]], the 2-point coreset that minimises
-        the MMD is specified y the indices S = [0, 2], ie X_c =  [[0,0], [[2,2]].
+        the MMD is specified by the indices S = [0, 2], ie X_c =  [[0,0], [[2,2]].
 
         Test, when given coreset indices [2,2], that refine_rand() returns [0, 2].
         """
 
         x = jnp.asarray([[0, 0], [1, 1], [2, 2]])
-        n = len(x)
 
         S = jnp.asarray([2, 2])
 
         K = vmap(
-            vmap(ck.rbf_kernel, in_axes=(None, 0), out_axes=0),
+            vmap(rbf_kernel, in_axes=(None, 0), out_axes=0),
             in_axes=(0, None),
             out_axes=0,
         )(x, x)
-        K_mean = K.sum(axis=1) / n
+        K_mean = K.mean(axis=1)
 
-        refine_test = cr.refine_rand(x, S, ck.rbf_kernel, K_mean, p=1.0)
+        refine_test = cr.refine_rand(x, S, rbf_kernel, K_mean, p=1.0)
 
         self.assertCountEqual(refine_test, jnp.asarray([0, 2]))
 
     def test_refine_rev(self):
         r"""
         For a toy example, X = [[0,0], [1,1], [2,2]], the 2-point coreset that minimises
-        the MMD is specified y the indices S = [0, 2], ie X_c =  [[0,0], [[2,2]].
+        the MMD is specified by the indices S = [0, 2], ie X_c =  [[0,0], [[2,2]].
 
         Test, when given coreset indices [1,2], that refine_rev() returns [0, 2].
         """
 
         x = jnp.asarray([[0, 0], [1, 1], [2, 2]])
-        n = len(x)
 
         S = jnp.asarray([1, 2])
 
         K = vmap(
-            vmap(ck.rbf_kernel, in_axes=(None, 0), out_axes=0),
+            vmap(rbf_kernel, in_axes=(None, 0), out_axes=0),
             in_axes=(0, None),
             out_axes=0,
         )(x, x)
-        K_mean = K.sum(axis=1) / n
+        K_mean = K.mean(axis=1)
 
-        refine_test = cr.refine_rev(x, S, ck.rbf_kernel, K_mean)
+        refine_test = cr.refine_rev(x, S, rbf_kernel, K_mean)
 
         self.assertCountEqual(refine_test, jnp.asarray([0, 2]))
