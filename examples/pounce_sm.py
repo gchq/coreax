fc0ebe4c14fc4544486b568492097609b285e061
tp832944 <139139729+tp832944@users.noreply.github.com>
2023-10-20 16:16:46 +0100

    fix: run pre-commit docstring check on examples
    Add docstring placeholders to modules in examples package.

--- examples/pounce_sm.py
+++ examples/pounce_sm.py
@@ -10,6 +10,8 @@
 # ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
 
+"""TODO: Write module docstring."""
+
 import os
 from pathlib import Path
 
