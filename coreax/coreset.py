55b88f5a0e0cb42fa928ef322f3f6fd828e9d7a9
tp832944 <139139729+tp832944@users.noreply.github.com>
2023-10-26 17:54:19 +0100

    feat: define Coreset ABC
    refs: #164

---
+++ coreax/coreset.py
@@ -0,0 +1,34 @@
+# © Crown Copyright GCHQ
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This module contains classes for methods of constructing coresets.
+
+Coresets are a type of data reduction, so these inherit from
+:class:`~coreax.reduction.DataReduction`. The aim is to select a samll set of indices
+that represent the key features of a larger dataset.
+
+The abstract base class is :class:`Coreset`. Concrete implementations are:
+
+*   :class:`KernelHerding` defines the kernel herding method for both regular and Stein
+    kernels.
+*   :class:`RandomSample` selects points for the coreset using random sampling. It is
+    typically only used for benchmarking against other coreset methods.
+"""
+
+from reduction import DataReduction
+
+
+class Coreset(DataReduction):
+    """Abstract base class for a method to construct a coreset."""
