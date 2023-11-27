c73c854f2c7a2326e07cd4abbbac4198ae9f5503
pc532627 <138115355+pc532627@users.noreply.github.com>
2023-11-27 15:47:00 +0000

    docs: examples updated with module docstrings.
    Refs: 29

--- coreax/metrics.py
+++ coreax/metrics.py
@@ -55,28 +55,28 @@ class Metric(ABC):
         Return a zero-dimensional array.
 
         :param x: An :math:`n \times d` array defining the full dataset
-        :param y: An :math:`n \times d` array defining a representation of x
+        :param y: An :math:`n \times d` array defining a representation of ``x``
         :param max_size: Size of matrix block to process
         :param weights_x: An :math:`1 \times n` array of weights for associated points
-            in x
+            in ``x``
         :param weights_y: An :math:`1 \times n` array of weights for associated points
-            in y
+            in ``y``
         :return: Metric computed as a zero-dimensional array.
         """
 
 
 class MMD(Metric):
     r"""
-    Calculation for maximum mean discrepancy between two datasets in d dimensions.
+    Calculation for maximum mean discrepancy between two datasets.
 
-    For dataset of n points in d dimensions, :math:`X`, and another dataset
-    :math:`Y` of m points in d dimensions, the maximum mean discrepancy is given
-    by:
+    For dataset of :math:`n` points in :math:`d` dimensions, :math:`X`, and another
+    dataset :math:`Y` of :math:`m` points in :math:`d` dimensions, the maximum mean
+    discrepancy is given by:
 
     .. math::
 
         \text{MMD}^2(X,Y) = \mathbb{E}(k(X,X)) + \mathbb{E}(k(Y,Y))
-        - 2\mathbb{E}(k(X,Y))
+        - 2\mathbb{E}(k(X,Y)).
 
     :param kernel: Kernel object with compute method defined mapping
         :math:`k: \mathbb{R}^d \times \mathbb{R}^d \rightarrow \mathbb{R}`
@@ -104,7 +104,7 @@ class MMD(Metric):
         r"""
         Calculate maximum mean discrepancy.
 
-        If no weights (for the dataset y) are given, standard MMD is calculated. If
+        If no weights (for the dataset ``y``) are given, standard MMD is calculated. If
         weights are given, weighted MMD is calculated. For both cases, if the size of
         matrix blocks to process is less than the size of both datasets, the calculation
         is done block-wise to limit memory requirements.
@@ -112,8 +112,8 @@ class MMD(Metric):
         :param x: The original :math:`n \times d` data
         :param y: :math:`m \times d` dataset
         :param max_size: (Optional) Size of matrix block to process, for memory limits
-        :param weights_x: (Optional)  :math:`n \times 1` weights for original data x
-        :param weights_y: (Optional)  :math:`m \times 1` weights for dataset y
+        :param weights_x: (Optional)  :math:`n \times 1` weights for original data ``x``
+        :param weights_y: (Optional)  :math:`m \times 1` weights for dataset ``y``
         :return: Maximum mean discrepancy as a 0-dimensional array
         """
         # Compute number of points in each input dataset
