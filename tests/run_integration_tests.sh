8aafa9d49bbf9fb2c618625b1f986637ae5bb7ce
tp832944 <139139729+tp832944@users.noreply.github.com>
2024-01-16 16:47:21 +0000

    feat: return pass/fail from integration test script
    Add that script must be called from repo root directory.
    
    refs: #167

--- tests/run_integration_tests.sh
+++ tests/run_integration_tests.sh
@@ -16,8 +16,29 @@
 
 # This script runs all integration tests, one by one, on the same process. This ensures
 # all tests can be run without JAX re-using JIT compiled functions outside of their
-# intended scope.
+# intended scope. It returns 1 if any test failed and 0 if all pass.
+#
+# This script must be run from the repository root directory.
 
-for filename in ./integration/test_*.py; do
-    python -m pytest $filename
+# Track number of tests and number of failures
+num_tests=0
+
+# Final total return code: 0 if successful; positive for a failure
+return_code=0
+
+for filename in ./tests/integration/test_*.py; do
+  num_tests=$((num_tests + 1))
+  pytest $filename
+  return_code=$((return_code + $?))
 done
+
+# Write blank line to aid readability of output
+echo
+
+if [ $return_code -eq 0 ]; then
+  echo "PASS: All ${num_tests} integration tests passed"
+  exit 0
+fi
+
+echo "FAILED: At least one failure out of ${num_tests} integration tests"
+exit 1
