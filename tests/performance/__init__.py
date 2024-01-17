625e2cea811824d0b7878ab0596436c7db2f6c8c
tl82649 <tl82649>
2024-01-17 14:31:21 +0000

    documenting structure and purpose of performance tests, and issues with tracer use in integration tests

--- tests/performance/__init__.py
+++ tests/performance/__init__.py
@@ -17,4 +17,17 @@ Tests to verify performance of the coreax library.
 
 Tests for specific functions, methods and classes in the coreax library, to ensure
 performance is as expected.
+
+The purpose of these tests is to ensure that Jax JIT applications are working as
+expected. If a JIT function or decorator is removed in development, e.g. for debugging,
+then these tests should catch this. Similarly, if JIT is being used unnecessarily, then
+this too should be caught. We recommend adding new tests here where use of JIT is
+present in the code.
+
+Test structure is as follows: call a function that makes use of JIT a number of times,
+forcing Jax to recompile it each time. Then call the function again for another number
+of times, allowing Jax to reuse the JIT tracers. Both of these sets of calls are timed,
+and the timings are supplied to a two-sample Kolmogorov-Smirnov test. If the timings are
+significantly different (where 'significant' is a configurable parameter in the test
+setUp), then the test passes; else it fails.
 """
