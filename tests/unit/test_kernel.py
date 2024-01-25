1ba10fa73898b70034b54675a15021953ea8f51a
bk958178 <144437611+bk958178@users.noreply.github.com>
2024-01-25 18:33:04 +0000

    create test_squared_exponential_div_x_grad_y_elementwise

--- tests/unit/test_kernel.py
+++ tests/unit/test_kernel.py
@@ -542,6 +542,24 @@ class TestSquaredExponentialKernel(unittest.TestCase):
         # Check output matches expected
         np.testing.assert_array_almost_equal(output, expected_output, decimal=3)
 
+    def test_squared_exponential_div_x_grad_y_elementwise(self) -> None:
+        # Setup data
+        length_scale = 1 / np.sqrt(2)
+        num_points = 1
+        dimension = 1
+        x = np.random.random((num_points, dimension))
+        y = np.random.random((num_points, dimension))
+
+        # Define expected output
+        dot_product = np.dot(x - y, x - y)
+        expected_output = 2 * np.exp(-dot_product) * (dimension - 2 * dot_product)
+
+        # Compute output using Kernel class
+        kernel = coreax.kernel.SquaredExponentialKernel(length_scale=length_scale)
+        output = kernel._divergence_x_grad_y_elementwise(x, y)
+
+        self.assertEqual(output, expected_output)
+
     def test_scaled_squared_exponential_div_x_grad_y(self) -> None:
         """
         Test the divergence w.r.t. ``x`` of kernel Jacobian w.r.t. ``y``; scaled.
