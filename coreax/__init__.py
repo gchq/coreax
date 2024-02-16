df0d270e7b163faeb4317843990bc0792b103d1e
tp832944 <139139729+tp832944@users.noreply.github.com>
2024-02-16 14:43:32 +0000

    chore: rename release 0.1.0-beta to 0.1.0

--- coreax/__init__.py
+++ coreax/__init__.py
@@ -57,7 +57,7 @@ loop over them individually. We currently do this using
 ``tests/integration/test_run.sh``.
 """
 
-__version__ = "0.1.0-beta"
+__version__ = "0.1.0"
 
 # pylint: disable=unused-import
 from coreax.coresubset import KernelHerding, RandomSample
