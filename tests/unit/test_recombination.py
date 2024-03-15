611f8da8b819baaf81f58f554cba479e7acf6ce7
tc85324 <158290903+tc85324@users.noreply.github.com>
2024-03-15 17:23:37 +0000

    feat: updte docstrings and tests

--- tests/unit/test_recombination.py
+++ tests/unit/test_recombination.py
@@ -1,12 +1,16 @@
 """Test recombination algorithms."""
 from __future__ import annotations
+from collections.abc import Callable, Iterator
 
 import warnings
-from typing import Literal
+from typing import Literal, get_args
+
+import equinox.internal as eqxi
 
 import jax
 import jax.numpy as jnp
 import jax.random as jr
+import jax.tree_util as jtu
 import numpy as np
 import pytest
 import scipy
@@ -17,144 +21,132 @@ from coreax.recombination import caratheodory_measure_reduction, reveal_null_spa
 jax.config.update("jax_enable_x64", True)
 InexactScalarLike = Inexact[ArrayLike, ""]
 
-
-# pylint: disable=line-too-long
-# Credit: https://github.com/FraCose/Recombination_Random_Algos/blob/2ca4ff74279eb1604376723dcb00ee65ff7e519a/recombination.py#L914
-# pylint: enable=line-too-long
-# The below implementation provides additional support for a Rank-Revealing QR mode
-# along with other quality of life changes.
-def _reference_caratheodory_measure_reduction(
-    weights: Shaped[np.ndarray, " n"],
-    nodes: Shaped[np.ndarray, "n d"],
-    rcond: InexactScalarLike | None = None,
-    mode: Literal["svd", "qr"] = "svd",
-) -> tuple[Shaped[np.ndarray, " d+1"], Shaped[np.ndarray, "d+1 d"]]:
-    augmented_nodes = np.insert(nodes, 0, 1.0, axis=1)
-    if mode == "svd":
-        _, s, vt = np.linalg.svd(augmented_nodes.T)
-        kernel_rank = reveal_null_space_rank(augmented_nodes, s, rcond)
-        null_basis = vt[-kernel_rank:, :].T
-    elif mode == "qr":
-        q, r, _ = scipy.linalg.qr(augmented_nodes, mode="full", pivoting=True)
-        kernel_rank = reveal_null_space_rank(augmented_nodes, np.abs(np.diag(r)), rcond)
-        null_basis = q[:, -kernel_rank:]
-    else:
-        msg = f"`mode` must be one of 'svd' or 'qr'; got {mode}"
-        raise ValueError(msg)
-
-    # pylint: disable=duplicate-code
-    if nodes.shape[0] <= kernel_rank:
-        warnings.warn(
-            "Nothing to do; node count already below reduction threshold",
-            stacklevel=2,
-        )
-        return weights, nodes
-    # pylint: enable=duplicate-code
-
-    for _ in range(kernel_rank):
-        null_vector = null_basis[:, 0]
-        # Ignore warnings from dividing zero by zero. These values are filtered out by
-        # the ``np.where`` clause.
-        with np.errstate(invalid="ignore"):
-            rescaling_factor = np.where(null_vector > 0, weights / null_vector, np.inf)
-        argmin_index = np.argmin(rescaling_factor)
-        weights = weights - rescaling_factor[argmin_index] * null_vector
-        weights[argmin_index] = 0.0
-
-        null_basis = np.delete(null_basis, 0, axis=1)
-        rescaled_null_vector = null_vector / null_vector[argmin_index]
-        null_basis -= np.tensordot(
-            rescaled_null_vector, null_basis[argmin_index], axes=0
-        )
-        null_basis[argmin_index, :] = 0.0
-
-    w_star = weights[weights > 0]
-    nodes_star = nodes[weights > 0]
-    return w_star, nodes_star
+N = 100
+D = 10
+# Rank (A) = M^\prime < M
+# Shape, colinear (subspace projection projection) and null are all types of 
+# rank-defficient degeneracy.
+# Convex is an alternative degeneracy.
+DEGENERACIES = Literal[None]  # , "shape", "colinear", "null", "convex"]
 
 
-@pytest.fixture(name="shaped_array", params=[(100, 10), (10, 100), (50, 1), (1, 50)])
-def fixture_shaped_array(request) -> Array:
-    """Fixture generates random arrays of varying shape."""
-    return jr.normal(jr.key(0), request.param)
+# TODO: need to be more precise with the degenerate atomic measure generation.
+@pytest.mark.parametrize("rcond", [None, 1e-6, 1e-12])
+@pytest.mark.parametrize("shape", [(N, D)])  # (D, D)
+class TestCase:
+    rng_seed = 0
 
+    @pytest.fixture
+    def atomic_measure_factory(self):
+        # TODO: docstring
+        key_generator = eqxi.GetKey(self.rng_seed)
 
-@pytest.fixture(
-    name="rcond", params=[None, np.asarray(1e-12), jnp.asarray([1e-12]), 1e-12]
-)
-def fixture_rcond(request):
-    """Fixture generates relative condition numbers of varying type."""
-    return request.param
+        def random_atomic_measure_generator(
+            shape: tuple[int, ...],
+            rcond: float | None,
+            *,
+            n_repeats: int = 5,
+            degeneracy: DEGENERACIES,
+        ):
+            # TODO: docstring
+            *_, n, d = shape
 
+            itteration_count = 0
+            while itteration_count < n_repeats:
+                weight_key, node_key = jr.split(key_generator())
+                weights = jr.uniform(weight_key, (shape[0],))
+                weights /= jnp.sum(weights)
+                assert jnp.all(weights >= 0) and jnp.isclose(jnp.sum(weights), 1.0)
+                nodes = jr.normal(node_key, shape)
 
-def test_reveal_null_space_rank(shaped_array, rcond):
-    """Test ``reveal_null_space_rank``."""
-    rank = np.linalg.matrix_rank(shaped_array, tol=rcond)
-    expected_null_space_rank = max(shaped_array.shape) - rank
-    _, s, _ = scipy.linalg.svd(shaped_array)
-    null_space_rank = reveal_null_space_rank(shaped_array, s, rcond)
-    assert null_space_rank == expected_null_space_rank
+                _matrix_rank = jtu.Partial(jnp.linalg.matrix_rank, tol=rcond)
+                is_degenerate = _matrix_rank(weights[..., None] * nodes) < d
+                if degeneracy is None:
+                    if is_degenerate:
+                        if n < d:
+                            raise ValueError(
+                                "It is impossible to generate a non-degenerate"
+                                f"(rank >= d) node matrix with shape '{shape}'"
+                            )
+                        continue
+                    pass
+                else:
+                    if degeneracy == "shape":
+                        weights = weights[: (d - 1)]
+                        nodes = nodes[: (d - 1)]
+                    elif degeneracy == "colinear":
+                        nodes = nodes.at[(d - 1) :].set(nodes.at[0].get())
+                    elif degeneracy == "null":
+                        nodes = nodes.at[(d - 1) :].set(0.0)
+                    else:
+                        raise ValueError(
+                            f"Degeneracy must be one of {get_args(DEGENERACIES)}; "
+                            f"got '{degeneracy}'"
+                        )
+                    is_degenerate = _matrix_rank(weights[..., None] * nodes) < d
+                    assert is_degenerate
+                yield weights, nodes
+                itteration_count += 1
 
+        return random_atomic_measure_generator
 
-# Add "qr" to mode once support is available in JAX.
-# see https://github.com/google/jax/issues/12897
-@pytest.mark.parametrize("mode", ["svd", "qr"])
-def test_caratheodory_measure_reduction_invariants(
-    shaped_array, rcond, mode: Literal["svd", "qr"]
-):
-    """
-    Test Caratheodory measure reduction preserves the centre-of-mass.
-    """
-    nodes = shaped_array
-    positive_weights = jr.uniform(jr.key(1234), (nodes.shape[0],))
-    probability_weights = positive_weights / np.sum(positive_weights)
-    expected_com = np.average(nodes, 0, weights=probability_weights)
+    @pytest.mark.parametrize("degeneracy", get_args(DEGENERACIES))
+    def test_reveal_null_space_rank(
+        self,
+        atomic_measure_factory: Callable[..., Iterator[tuple[Array, Array]]],
+        shape: tuple[int, ...],
+        rcond: float | None,
+        degeneracy: DEGENERACIES,
+    ):
+        for _, nodes in atomic_measure_factory(shape, rcond, degeneracy=degeneracy):
+            rank = np.linalg.matrix_rank(nodes, tol=rcond)
+            expected_null_space_rank = max(nodes.shape) - rank  # TODO: fix test
+            _, s, _ = scipy.linalg.svd(nodes)
+            null_space_rank = reveal_null_space_rank(nodes, s, rcond)
+            assert null_space_rank == expected_null_space_rank
 
-    with warnings.catch_warnings(record=True) as record:
-        warnings.simplefilter("always")
-        reference_weights, reference_nodes = _reference_caratheodory_measure_reduction(
-            np.asarray(probability_weights), np.asarray(nodes), rcond=rcond, mode=mode
-        )
-        caratheodory_weights, caratheodory_nodes = caratheodory_measure_reduction(
-            probability_weights, nodes, rcond=rcond, mode=mode
-        )
-
-        if nodes.shape[0] < nodes.shape[1]:
-            expected_warning_count = 2
-            assert len(record) == expected_warning_count
-            assert all("Nothing to do" in str(warn.message) for warn in record)
-            expected_weights_shape = probability_weights.shape
-            expected_nodes_shape = nodes.shape
-        else:
+    @pytest.mark.parametrize("mode", ["svd"])
+    @pytest.mark.parametrize(
+        "assume_non_degenerate,degeneracy",
+        [(False, i) for i in get_args(DEGENERACIES)] + [(True, None)],
+    )
+    def test_caratheodory_measure_reduction(
+        self,
+        atomic_measure_factory: Callable[..., Iterator[tuple[Array, Array]]],
+        shape: tuple[int, ...],
+        rcond: float | None,
+        mode: Literal["svd", "qr"],
+        assume_non_degenerate: bool,
+        degeneracy: DEGENERACIES,
+    ):
+        case_iterator = atomic_measure_factory(shape, rcond, degeneracy=degeneracy)
+        for weights, nodes in case_iterator:
+            with warnings.catch_warnings(record=True) as record:
+                warnings.simplefilter("always")
+                result_weights, result_nodes = caratheodory_measure_reduction(
+                    weights,
+                    nodes,
+                    rcond=rcond,
+                    mode=mode,
+                    assume_non_degenerate=assume_non_degenerate,
+                )
+            # if degeneracy == "shape":
+            #     expected_warning_count = 2
+            #     assert len(record) == expected_warning_count
+            #     assert all("Nothing to do" in str(warn.message) for warn in record)
+            #     expected_weights_shape = weights.shape
+            #     expected_nodes_shape = nodes.shape
+            # else:
             expected_weights_shape = (nodes.shape[-1] + 1,)
             expected_nodes_shape = (nodes.shape[-1] + 1, nodes.shape[-1])
 
-    # Check reference implementation
-    reference_com = np.average(reference_nodes, 0, weights=reference_weights)
-    assert reference_weights.shape == expected_weights_shape
-    assert reference_nodes.shape == expected_nodes_shape
-    np.testing.assert_almost_equal(np.sum(reference_weights), 1.0)
-    np.testing.assert_array_almost_equal(reference_com, expected_com)
+            result_com = np.average(result_nodes, 0, weights=result_weights)
 
-    # Weak sense equivalence to the reference implementation
-    recombined_com = np.average(caratheodory_nodes, 0, weights=caratheodory_weights)
-    np.testing.assert_almost_equal(np.sum(caratheodory_weights), 1.0)
-    np.testing.assert_array_almost_equal(recombined_com, reference_com)
+            expected_weights_shape = (nodes.shape[-1] + 1,)
+            expected_nodes_shape = (nodes.shape[-1] + 1, nodes.shape[-1])
+            expected_com = np.average(nodes, 0, weights=weights)
 
-    # Strong sense equivalence to the reference implementation
-    # IMPORTANT: we only expect strong equivalence if the implementations are, for
-    # all intents and purposes, identical. There exist many (non-unique) solutions
-    # to the caratheodory measure reduction problem which, for the purposes
-    # considered here, are equivalent; we want *any* basic feasible solution to
-    # the underlying linear programming (optimisation) problem, where the objective
-    # function is trivial. E.G. max c^T x, with c = 0.
-    assert caratheodory_weights.shape == reference_weights.shape
-    assert caratheodory_nodes.shape == reference_nodes.shape
-    np.testing.assert_array_almost_equal(caratheodory_weights, reference_weights)
-    np.testing.assert_array_almost_equal(caratheodory_nodes, reference_nodes)
-
-
-# def test_rank_degenerate_invariants(self):
-#     ...
-
-# Test jit and differentiability in forward and reverse mode.
+            assert result_weights.shape == expected_weights_shape
+            assert result_nodes.shape == expected_nodes_shape
+            np.testing.assert_almost_equal(np.sum(result_weights), 1.0)
+            np.testing.assert_array_almost_equal(result_com, expected_com)
