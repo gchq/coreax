b1b57bbaa5f9e562c19c51f89f86b44c16ad88a0
db091756 <167978891+db091756@users.noreply.github.com>
2024-05-10 10:00:22 +0100

    Update coreax/kernel.py
    Co-authored-by: tc85324 <158290903+tc85324@users.noreply.github.com>

--- coreax/kernel.py
+++ coreax/kernel.py
@@ -341,7 +341,7 @@ class SquaredExponentialKernel(Kernel):
     Given :math:`\lambda =`'length_scale' and :math:`\rho =`'output_scale', the squared
     exponential kernel is defined as
     :math:`k: \mathbb{R}^d\times \mathbb{R}^d \to \mathbb{R}`,
-    :math:`k(x, y) = \rho \exp(\frac{||x-y||^2}{2 \lambda^2})` where
+    :math:`k(x, y) = \rho * \exp(\frac{||x-y||^2}{2 \lambda^2})` where
     :math:`||\cdot||` is the usual :math:`L_2`-norm.
 
     :param length_scale: Kernel smoothing/bandwidth parameter, :math:`\lambda`
