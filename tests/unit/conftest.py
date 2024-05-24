ea896bd04d7790e6c042a98edbe4fe79918c30ee
tc85324 <158290903+tc85324@users.noreply.github.com>
2024-05-24 11:12:48 +0000

    feat: replace performance test with jit_variants
    Simplifies implementation of #581. The performance tests used to check
    if JIT compilation significantly reduced the runtime of kernel methods
    and some coreset algorithms. Significance was determined by a KS test
    between the distribution of repeated pre and post JIT run-times.
    
    While knowing if JIT compilation enhances performance is useful, it is
    not a strict property of our methods. The critical check is that the
    methods can be successfully JIT compiled (a now necessary property).
    
    The impact of JIT compilation on runtime performance should be covered
    by a future pull to address #509 (performance benchmarking).
    
    Refs: #509, #581

---
+++ tests/unit/conftest.py
@@ -0,0 +1,30 @@
+# © Crown Copyright GCHQ
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Fixtures used across multiple unit tests modules."""
+
+from collections.abc import Callable
+
+import equinox as eqx
+import pytest
+
+
+@pytest.fixture(params=["with_jit", "without_jit"], scope="class")
+def jit_variant(request) -> Callable[[Callable], Callable]:
+    """Return a callable that (may) JIT compile a passed callable."""
+    if request.param == "without_jit":
+        return lambda x: x
+    if request.param == "with_jit":
+        return eqx.filter_jit
+    raise ValueError("Invalid fixture parametrization.")
