cf2c9dee169332ce68fa10f5b5c7f283840b2321
tc85324 <158290903+tc85324@users.noreply.github.com>
2024-05-24 07:53:19 +0000

    feat: remove `DataReader` API - code pass
    Required to implement #581. Only covers functional code and tests.
    Documentation will be covered in a separate pass.
    
    Refs: #581

--- tests/unit/test_data.py
+++ tests/unit/test_data.py
@@ -19,17 +19,12 @@ The tests within this file verify that approaches to handling and processing dat
 produce the expected results on simple examples.
 """
 
-import unittest
 from functools import partial
-from unittest.mock import MagicMock
 
 import equinox as eqx
 import jax.numpy as jnp
 import jax.tree_util as jtu
-import numpy as np
 import pytest
-from jax import Array
-from jax.typing import ArrayLike
 
 import coreax.data
 import coreax.reduction
@@ -124,59 +119,3 @@ class TestSupervisedData:
         ):
             invalid_supervision = jnp.ones(DATA_ARRAY.shape[0])
             coreax.data.SupervisedData(DATA_ARRAY, invalid_supervision)
-
-
-class DataReaderConcrete(coreax.data.DataReader):
-    """Concrete implementation of DataReader class to allow testing."""
-
-    @classmethod
-    def load(cls, original_data: ArrayLike) -> coreax.data.DataReader:
-        raise NotImplementedError
-
-    def format(self, coreset: coreax.reduction.Coreset) -> Array:
-        raise NotImplementedError
-
-
-class TestDataReader(unittest.TestCase):
-    """Test operation of DataReader class."""
-
-    def test_init_scalars(self):
-        """Test that scalars are cast properly."""
-        actual = DataReaderConcrete(original_data=1, pre_coreset_array=2)
-        self.assertEqual(actual.original_data, jnp.array(1))
-        self.assertEqual(actual.pre_coreset_array, jnp.array([[2]]))
-
-    def test_non_abstract_methods_raise(self):
-        """
-        Test calls to the non-abstract methods of DataReader raise the expected errors.
-        """
-        reader_object = DataReaderConcrete(original_data=1, pre_coreset_array=2)
-        self.assertRaises(NotImplementedError, reader_object.render, MagicMock())
-        self.assertRaises(NotImplementedError, reader_object.reduce_dimension, 2)
-        self.assertRaises(
-            NotImplementedError, reader_object.restore_dimension, MagicMock()
-        )
-
-
-class TestArrayData(unittest.TestCase):
-    """Test ArrayData class."""
-
-    def test_load(self):
-        """Check that no preprocessing is done during load."""
-        original_data = jnp.array([[1, 2]])
-        actual = coreax.data.ArrayData.load(original_data)
-        np.testing.assert_array_equal(actual.original_data, original_data)
-        np.testing.assert_array_equal(actual.pre_coreset_array, original_data)
-
-    def test_format(self):
-        """Check that coreset is returned without further formatting."""
-        original_data = jnp.array([[2, 3], [4, 5], [6, 7]])
-        coreset_array = jnp.array([[2, 3], [4, 5]])
-        coreset_obj = MagicMock()
-        coreset_obj.coreset = coreset_array
-        data_reader = coreax.data.ArrayData(original_data, original_data)
-        np.testing.assert_array_equal(data_reader.format(coreset_obj), coreset_array)
-
-
-if __name__ == "__main__":
-    unittest.main()
