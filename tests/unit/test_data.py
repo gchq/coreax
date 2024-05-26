fix: handle normalization of zero weights
Provides handling for weight normalization when some/all weights are
zero by providing the `preserve_zeros` keyword for `Data.normalize(...)`

tests/unit/test_data.py
@@ -89,13 +89,18 @@ class TestData:
         _data = data_type()
         assert len(_data) == len(_data.data)
 
-    @pytest.mark.parametrize("weights", (None, 3, DATA_ARRAY.reshape(-1)))
+    @pytest.mark.parametrize("weights", (None, 0, 3, DATA_ARRAY.reshape(-1)))
     def test_normalize(self, data_type, weights):
         """Test weight normalization."""
         data = data_type(weights)
         expected_weights = data.weights / jnp.sum(data.weights)
-        normalized_data = data.normalize()
-        assert eqx.tree_equal(normalized_data.weights, expected_weights)
+        if jnp.all(weights != 0):
+            normalized_data = data.normalize()
+            assert eqx.tree_equal(normalized_data.weights, expected_weights)
+        normalized_with_zeros = data.normalize(preserve_zeros=True)
+        assert eqx.tree_equal(
+            normalized_with_zeros.weights, jnp.nan_to_num(expected_weights)
+        )
 
 
 class TestSupervisedData: