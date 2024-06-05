b19ce25f2099130bbc3cba11696b70b64a64de05
db091756 <167978891+db091756@users.noreply.github.com>
2024-06-05 09:54:38 +0100

    Feat/more kernels (#616)
    * feat: refactored linear kernel and added Rational Quadratic
    
    * feat: added periodic kernel, need to raise error on gradient computation
    
    * feat: added more kernels
    
    * feat: added divergence to polynomial kernel and did some minor refactoring
    
    * tests: added tests for all additional kernels
    
    * refactor: small type error and added own error class for non-differentiability
    
    * feat: removed non-differentiability errors and implemented gradients and divergences with corresponding tests
    
    * updated changelog
    
    * tests: added tests to check auto-differentiation methods agree with overridden implementations
    
    * fix: removed spurious print statement
    
    * feat: update changelog
    
    * tests: removed limit test as float 32 is not accurate enough
    
    * refactor: rebased to fixed kernel composition code
    
    * docs: added non-differentiability note
    
    * tests: added tests for magic methods
    
    * fix: fixed unit test
    
    * refactor: responded to reviewer comments
    
    * ignore bad type complaint
    
    * tests: added polynomial kernel test
    
    * docs: small doc update

--- coreax/kernel.py
+++ coreax/kernel.py
@@ -553,7 +553,7 @@ class LinearKernel(Kernel):
     r"""
     Define a linear kernel.
 
-    Given :math:`\rho =`'output_scale' and :math:`c =`'constant',  the linear kernel is
+    Given :math:`\rho`=`'output_scale` and :math:`c =`'constant',  the linear kernel is
     defined as :math:`k: \mathbb{R}^d\times \mathbb{R}^d \to \mathbb{R}`,
     :math:`k(x, y) = \rho x^Ty + c`.
 
@@ -578,17 +578,80 @@ class LinearKernel(Kernel):
 
     @override
     def divergence_x_grad_y_elementwise(self, x: ArrayLike, y: ArrayLike) -> Array:
-        return self.output_scale * jnp.asarray(jnp.shape(x)[0])
+        d = len(jnp.asarray(x))
+        return jnp.array(self.output_scale * d)
+
+
+class PolynomialKernel(Kernel):
+    r"""
+    Define a polynomial kernel.
+
+    Given :math:`\rho =``output_scale`, :math:`c =`'constant', and :math:`d=`'degree',
+    the polynomial kernel is defined as
+    :math:`k: \mathbb{R}^d\times \mathbb{R}^d \to \mathbb{R}`,
+    :math:`k(x, y) = \rho (x^Ty + c)^d`.
+
+    :param output_scale: Kernel normalisation constant, :math:`\rho`
+    :param constant: Additive constant, :math:`c`
+    :param degree: Degree of kernel, must be a positive integer greater than 1.
+    """
+
+    output_scale: float = 1.0
+    constant: float = 0.0
+    degree: int = 2
+
+    def __check_init__(self):
+        """Ensure degree is an integer greater than 1."""
+        min_degree = 2
+        if not isinstance(self.degree, int) or self.degree < min_degree:
+            raise ValueError("'degree' must be a positive integer greater than 1")
+
+    @override
+    def compute_elementwise(self, x: ArrayLike, y: ArrayLike) -> Array:
+        return self.output_scale * (jnp.dot(x, y) + self.constant) ** self.degree
+
+    @override
+    def grad_x_elementwise(self, x: ArrayLike, y: ArrayLike) -> Array:
+        return (
+            self.output_scale
+            * self.degree
+            * jnp.asarray(y)
+            * (jnp.dot(x, y) + self.constant) ** (self.degree - 1)
+        )
+
+    @override
+    def grad_y_elementwise(self, x: ArrayLike, y: ArrayLike) -> Array:
+        return (
+            self.output_scale
+            * self.degree
+            * jnp.asarray(x)
+            * (jnp.dot(x, y) + self.constant) ** (self.degree - 1)
+        )
+
+    @override
+    def divergence_x_grad_y_elementwise(self, x: ArrayLike, y: ArrayLike) -> Array:
+        dot = jnp.dot(x, y)
+        body = dot + self.constant
+        d = len(jnp.asarray(x))
+
+        return (
+            self.output_scale
+            * self.degree
+            * (
+                ((self.degree - 1) * dot * body ** (self.degree - 2))
+                + (d * body ** (self.degree - 1))
+            )
+        )
 
 
 class SquaredExponentialKernel(Kernel):
     r"""
     Define a squared exponential kernel.
 
-    Given :math:`\lambda =`'length_scale' and :math:`\rho =`'output_scale', the squared
+    Given :math:`\lambda =``length_scale` and :math:`\rho =``output_scale`, the squared
     exponential kernel is defined as
     :math:`k: \mathbb{R}^d\times \mathbb{R}^d \to \mathbb{R}`,
-    :math:`k(x, y) = \rho * \exp(\frac{||x-y||^2}{2 \lambda^2})` where
+    :math:`k(x, y) = \rho * \exp(-\frac{||x-y||^2}{2 \lambda^2})` where
     :math:`||\cdot||` is the usual :math:`L_2`-norm.
 
     :param length_scale: Kernel smoothing/bandwidth parameter, :math:`\lambda`
@@ -622,14 +685,251 @@ class SquaredExponentialKernel(Kernel):
         return scale * k * (d - scale * squared_distance(x, y))
 
 
+class ExponentialKernel(Kernel):
+    r"""
+    Define an exponential kernel.
+
+    Given :math:`\lambda =``length_scale` and :math:`\rho =``output_scale`, the
+    exponential kernel is defined as
+    :math:`k: \mathbb{R}^d\times \mathbb{R}^d \to \mathbb{R}`,
+    :math:`k(x, y) = \rho * \exp(-\frac{||x-y||}{2 \lambda^2})` where
+    :math:`||\cdot||` is the usual :math:`L_2`-norm.
+
+    .. note::
+        Note that the Exponential kernel is not differentiable when :math:`x=y`.
+
+    :param length_scale: Kernel smoothing/bandwidth parameter, :math:`\lambda`
+    :param output_scale: Kernel normalisation constant, :math:`\rho`
+    """
+
+    length_scale: float = 1.0
+    output_scale: float = 1.0
+
+    @override
+    def compute_elementwise(self, x: ArrayLike, y: ArrayLike) -> Array:
+        return self.output_scale * jnp.exp(
+            -jnp.linalg.norm(jnp.subtract(x, y)) / (2 * self.length_scale**2)
+        )
+
+    @override
+    def grad_x_elementwise(self, x: ArrayLike, y: ArrayLike) -> Array:
+        return -self.grad_y_elementwise(x, y)
+
+    @override
+    def grad_y_elementwise(self, x: ArrayLike, y: ArrayLike) -> Array:
+        sub = jnp.subtract(x, y)
+        dist = jnp.linalg.norm(sub)
+        factor = 2 * self.length_scale**2
+        return self.output_scale * sub * jnp.exp(-dist / factor) / (factor * dist)
+
+    @override
+    def divergence_x_grad_y_elementwise(self, x: ArrayLike, y: ArrayLike) -> Array:
+        d = len(jnp.asarray(x))
+        sub = jnp.subtract(x, y)
+        dist = jnp.linalg.norm(sub)
+        factor = 2 * self.length_scale**2
+        exp = jnp.exp(-dist / factor)
+
+        first_term = (-exp * sub / dist**2) * ((1 / dist) + 1 / factor)
+        second_term = exp / dist
+
+        return (self.output_scale / factor) * (
+            jnp.dot(first_term, sub) + d * second_term
+        )
+
+
+class RationalQuadraticKernel(Kernel):
+    r"""
+    Define a rational quadratic kernel.
+
+    Given :math:`\lambda =``length_scale`,  :math:`\rho =``output_scale`, and
+    :math:`\alpha =``relative_weighting`, the rational quadratic kernel is defined as
+    :math:`k: \mathbb{R}^d\times \mathbb{R}^d \to \mathbb{R}`,
+    :math:`k(x, y) = \rho * (1 + \frac{||x-y||^2}{2 \alpha \lambda^2})^{-\alpha}` where
+    :math:`||\cdot||` is the usual :math:`L_2`-norm.
+
+    :param length_scale: Kernel smoothing/bandwidth parameter, :math:`\lambda`
+    :param output_scale: Kernel normalisation constant, :math:`\rho`
+    :param relative_weighting: Parameter controlling the relative weighting of
+        large-scale and small-scale variations, :math:`\alpha`. As
+        :math:`alpha \to \infty` the rational quadratic kernel is identical to the
+        squared exponential kernel.
+    """
+
+    length_scale: float = 1.0
+    output_scale: float = 1.0
+    relative_weighting: float = 1.0
+
+    @override
+    def compute_elementwise(self, x: ArrayLike, y: ArrayLike) -> Array:
+        return (
+            self.output_scale
+            * (
+                1
+                + squared_distance(x, y)
+                / (2 * self.relative_weighting * self.length_scale**2)
+            )
+            ** -self.relative_weighting
+        )
+
+    @override
+    def grad_x_elementwise(self, x: ArrayLike, y: ArrayLike) -> Array:
+        return -self.grad_y_elementwise(x, y)
+
+    @override
+    def grad_y_elementwise(self, x: ArrayLike, y: ArrayLike) -> Array:
+        return (self.output_scale * jnp.subtract(x, y) / self.length_scale**2) * (
+            1
+            + squared_distance(x, y)
+            / (2 * self.relative_weighting * self.length_scale**2)
+        ) ** (-self.relative_weighting - 1)
+
+    @override
+    def divergence_x_grad_y_elementwise(self, x: ArrayLike, y: ArrayLike) -> Array:
+        d = len(jnp.asarray(x))
+        sq_dist = squared_distance(x, y)
+        power = self.relative_weighting + 1
+        div = self.relative_weighting * self.length_scale**2
+        body = 1 + sq_dist / (2 * div)
+        factor = self.output_scale / self.length_scale**2
+
+        first_term = factor * body**-power
+        second_term = -(factor * power * sq_dist / div) * body ** -(power + 1)
+        return d * first_term + second_term
+
+
+class PeriodicKernel(Kernel):
+    r"""
+    Define a periodic kernel.
+
+    Given :math:`\lambda =``length_scale`,  :math:`\rho =``output_scale`, and
+    :math:`\p =``periodicity`, the periodic kernel is defined as
+    :math:`k: \mathbb{R}^d\times \mathbb{R}^d \to \mathbb{R}`,
+    :math:`k(x, y) = \rho * \exp(\frac{-2 \sin^2(\pi ||x-y||/p)}{\lambda^2})` where
+    :math:`||\cdot||` is the usual :math:`L_2`-norm.
+
+    .. note::
+        Note that the Periodic kernel is not differentiable when :math:`x=y`.
+
+    :param length_scale: Kernel smoothing/bandwidth parameter, :math:`\lambda`
+    :param output_scale: Kernel normalisation constant, :math:`\rho`
+    :param periodicity: Parameter controlling the periodicity of the kernel. :\math: `p`
+    """
+
+    length_scale: float = 1.0
+    output_scale: float = 1.0
+    periodicity: float = 1.0
+
+    @override
+    def compute_elementwise(self, x: ArrayLike, y: ArrayLike) -> Array:
+        return self.output_scale * (
+            jnp.exp(
+                -2
+                * jnp.sin(
+                    jnp.pi * jnp.linalg.norm(jnp.subtract(x, y)) / self.periodicity
+                )
+                ** 2
+                / self.length_scale**2
+            )
+        )
+
+    @override
+    def grad_x_elementwise(self, x: ArrayLike, y: ArrayLike) -> Array:
+        return -self.grad_y_elementwise(x, y)
+
+    @override
+    def grad_y_elementwise(self, x: ArrayLike, y: ArrayLike) -> Array:
+        dist = jnp.linalg.norm(jnp.subtract(x, y))
+        body = jnp.pi * dist / self.periodicity
+        return (
+            (
+                4
+                * jnp.subtract(x, y)
+                * self.output_scale
+                * jnp.pi
+                / (dist * self.periodicity * self.length_scale**2)
+            )
+            * jnp.sin(body)
+            * jnp.cos(body)
+            * jnp.exp(-(2 / self.length_scale**2) * jnp.sin(body) ** 2)
+        )
+
+    @override
+    def divergence_x_grad_y_elementwise(self, x: ArrayLike, y: ArrayLike) -> Array:
+        d = len(jnp.asarray(x))
+        sub = jnp.subtract(x, y)
+        dist = jnp.linalg.norm(sub)
+        factor = jnp.pi / self.periodicity
+        func_body = factor * dist
+        grad_factor = sub / dist
+        output_factor = 4 * factor * self.output_scale / self.length_scale**2
+
+        func_1 = 1 / dist
+        func_2 = jnp.sin(func_body)
+        func_3 = jnp.cos(func_body)
+        func_4 = jnp.exp(-(2 / self.length_scale**2) * func_2**2)
+
+        first_term = func_1 * func_2 * func_3 * func_4
+        second_term = (
+            -grad_factor * func_1**2 * func_2 * func_3 * func_4
+            - grad_factor * factor * func_1 * func_2**2 * func_4
+            + grad_factor * factor * func_1 * func_3**2 * func_4
+            - (output_factor * sub * func_1**2 * func_2**2 * func_3**2 * func_4)
+        )
+
+        return output_factor * (d * first_term + jnp.dot(second_term, sub))
+
+
+class LocallyPeriodicKernel(ProductKernel):
+    r"""
+    Define a locally periodic kernel.
+
+    The periodic kernel is defined as
+    :math:`k: \mathbb{R}^d\times \mathbb{R}^d \to \mathbb{R}`,
+    :math:`k(x, y) = r(x,y)l(x,y)` where :math:`r` is the periodic kernel and
+    :math:`l` is the squared exponential kernel.
+
+    .. note::
+        Note that the Periodic kernel is not differentiable when :math:`x=y`.
+
+    :param periodic_length_scale: Periodic kernel smoothing/bandwidth parameter
+    :param periodic_output_scale: Periodic kernel normalisation constant
+    :param periodicity: Parameter controlling the periodicity of the Periodic kernel
+    :param squared_exponential_length_scale: SquaredExponential kernel
+        smoothing/bandwidth parameter]
+    :param squared_exponential_output_scale: SquaredExponential Kernel normalisation
+        constant
+    """
+
+    def __init__(
+        self,
+        periodic_length_scale: float = 1.0,
+        periodic_output_scale: float = 1.0,
+        periodicity: float = 1.0,
+        squared_exponential_length_scale: float = 1.0,
+        squared_exponential_output_scale: float = 1.0,
+    ):
+        """Initialise LocallyPeriodicKernel with ProductKernel attributes."""
+        self.first_kernel = PeriodicKernel(
+            length_scale=periodic_length_scale,
+            output_scale=periodic_output_scale,
+            periodicity=periodicity,
+        )
+
+        self.second_kernel = SquaredExponentialKernel(
+            length_scale=squared_exponential_length_scale,
+            output_scale=squared_exponential_output_scale,
+        )
+
+
 class LaplacianKernel(Kernel):
     r"""
     Define a Laplacian kernel.
 
-    Given :math:`\lambda =`'length_scale' and :math:`\rho =`'output_scale', the
+    Given :math:`\lambda =``length_scale` and :math:`\rho =``output_scale`, the
     Laplacian kernel is defined as
     :math:`k: \mathbb{R}^d\times \mathbb{R}^d \to \mathbb{R}`,
-    :math:`k(x, y) = \rho * \exp(\frac{||x-y||_1}{2 \lambda^2})`  where
+    :math:`k(x, y) = \rho * \exp(-\frac{||x-y||_1}{2 \lambda^2})`  where
     :math:`||\cdot||_1` is the :math:`L_1`-norm.
 
     :param length_scale: Kernel smoothing/bandwidth parameter, :math:`\lambda`
@@ -668,7 +968,7 @@ class PCIMQKernel(Kernel):
     r"""
     Define a pre-conditioned inverse multi-quadric (PCIMQ) kernel.
 
-    Given :math:`\lambda =`'length_scale' and :math:`\rho =`'output_scale', the
+    Given :math:`\lambda =``length_scale` and :math:`\rho =``output_scale`, the
     PCIMQ kernel is defined as
     :math:`k: \mathbb{R}^d\times \mathbb{R}^d \to \mathbb{R}`,
     :math:`k(x, y) = \frac{\rho}{\sqrt{1 + \frac{||x-y||^2}{2 \lambda^2}}}
