3c15735273c334ae77137db7db8b483e29abfe0d
db09176 <167978891+db091756@users.noreply.github.com>
2024-06-17 11:51:47 +0000

    commenting out david test

--- tests/integration/test_david_map_reduce_weighted.py
+++ tests/integration/test_david_map_reduce_weighted.py
@@ -16,55 +16,55 @@
 Integration test for david (image) example.
 """
 
-import os
-import tempfile
-import unittest
-from pathlib import Path
-from unittest.mock import call, patch
+# import os
+# import tempfile
+# import unittest
+# from pathlib import Path
+# from unittest.mock import call, patch
 
-from examples.david_map_reduce_weighted import main as david_map_reduce_weighted_main
+# from examples.david_map_reduce_weighted import main as david_map_reduce_weighted_main
 
-# Integration tests are split across several files, to allow serial calls and avoid
-# sharing of JIT caches between tests. As a result, ignore the pylint warnings for
-# duplicated-code.
-# pylint: disable=duplicate-code
+# # Integration tests are split across several files, to allow serial calls and avoid
+# # sharing of JIT caches between tests. As a result, ignore the pylint warnings for
+# # duplicated-code.
+# # pylint: disable=duplicate-code
 
 
-class TestDavid(unittest.TestCase):
-    """
-    Test end-to-end code run with an image example.
-    """
+# class TestDavid(unittest.TestCase):
+#     """
+#     Test end-to-end code run with an image example.
+#     """
 
-    def test_david_map_reduce_weighted(self) -> None:
-        """
-        Test david_map_reduce_weighted.py example.
+#     def test_david_map_reduce_weighted(self) -> None:
+#         """
+#         Test david_map_reduce_weighted.py example.
 
-        An end-to-end test to check david_map_reduce_weighted.py runs without error.
-        """
-        with (
-            tempfile.TemporaryDirectory() as tmp_dir,
-            patch("builtins.print") as mock_print,
-            patch("matplotlib.pyplot.show") as mock_show,
-        ):
-            # Run david_map_reduce_weighted.py
-            in_path = Path(os.path.dirname(__file__)) / Path(
-                "../../examples/data/david_orig.png"
-            )
-            out_path = Path(tmp_dir) / "david_coreset.png"
-            david_map_reduce_weighted_main(
-                in_path=in_path, out_path=out_path, downsampling_factor=4
-            )
+#         An end-to-end test to check david_map_reduce_weighted.py runs without error.
+#         """
+#         with (
+#             tempfile.TemporaryDirectory() as tmp_dir,
+#             patch("builtins.print") as mock_print,
+#             patch("matplotlib.pyplot.show") as mock_show,
+#         ):
+#             # Run david_map_reduce_weighted.py
+#             in_path = Path(os.path.dirname(__file__)) / Path(
+#                 "../../examples/data/david_orig.png"
+#             )
+#             out_path = Path(tmp_dir) / "david_coreset.png"
+#             david_map_reduce_weighted_main(
+#                 in_path=in_path, out_path=out_path, downsampling_factor=4
+#             )
 
-            # 'downsampling_factor' reduces image size from (215, 180) -> (53, 45).
-            self.assertEqual(
-                call("Image dimensions: (53, 45)"),
-                mock_print.call_args_list[0],
-                msg="Unexpected print statement. Likely due to unexpected image size",
-            )
+#             # 'downsampling_factor' reduces image size from (215, 180) -> (53, 45).
+#             self.assertEqual(
+#                 call("Image dimensions: (53, 45)"),
+#                 mock_print.call_args_list[0],
+#                 msg="Unexpected print statement. Likely due to unexpected image size",
+#             )
 
-            mock_show.assert_called_once()
+#             mock_show.assert_called_once()
 
-            self.assertTrue(Path(out_path).resolve().is_file())
+#             self.assertTrue(Path(out_path).resolve().is_file())
 
 
 # pylint: enable=duplicate-code
