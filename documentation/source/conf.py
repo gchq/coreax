a53cfb449f3071058dac6613d8c1ed7753336d6c
tp832944 <139139729+tp832944@users.noreply.github.com>
2024-06-20 17:59:19 +0100

    fix: add numpy.bool_ to docs nitpick ignore
    refs: #671

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -169,6 +169,9 @@ nitpick_ignore = [
     ("py:class", "jaxtyping.Shaped[Array, 'n']"),
     ("py:class", "jaxtyping.Shaped[ndarray, 'n']"),
     ("py:class", "jax._src.typing.SupportsDType"),
+    # TODO: Remove once no longer supporting Numpy < 2
+    # https://github.com/gchq/coreax/issues/674
+    ("py:class", "numpy.bool_"),
 ]
 
 
