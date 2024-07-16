acb19c51b2af2c5f322f305b39fbc12d60bacd41
db09176 <167978891+db091756@users.noreply.github.com>
2024-07-16 13:22:54 +0000

    fix: fix tests and some doc changes

--- coreax/solvers/coresubset.py
+++ coreax/solvers/coresubset.py
@@ -487,7 +487,7 @@ def _greedy_kernel_inducing_points_loss(
     feature_gramian: Array,
     regularisation_parameter: float,
     identity: Array,
-    inverse_approximator: RegularisedLeastSquaresSolver,
+    least_square_solver: RegularisedLeastSquaresSolver,
 ) -> Array:
     """
     Given an array of candidate coreset indices, compute the greedy KIP loss for each.
@@ -501,7 +501,7 @@ def _greedy_kernel_inducing_points_loss(
     :param regularisation_parameter: Regularisation parameter for stable inversion of
         array, negative values will be converted to positive
     :param identity: identity matrix
-    :param inverse_approximator: Instance of
+    :param least_square_solver: Instance of
         :class:`coreax.least_squares.RegularisedLeastSquaresSolver`
 
     :return: GreedyKernelInducingPoints loss for each candidate coreset
@@ -514,16 +514,15 @@ def _greedy_kernel_inducing_points_loss(
     coreset_cross_gramians = feature_gramian[candidate_coresets, :-1]
     coreset_responses = responses[candidate_coresets]
 
-    # Invert the coreset feature gramians
-    inverse_coreset_gramians = inverse_approximator.solve_stack(
-        coreset_gramians,
-        regularisation_parameter,
-        identity,
-        identity,
+    # Solve for the kernel ridge regression coefficients for each possible coreset
+    coefficients = least_square_solver.solve_stack(
+        arrays=coreset_gramians,
+        regularisation_parameter=regularisation_parameter,
+        targets=coreset_responses,
+        identity=identity,
     )
 
     # Compute the loss function
-    coefficients = inverse_coreset_gramians @ coreset_responses
     predictions = (coreset_cross_gramians * coefficients).sum(axis=1)
     term_2s = (predictions**2).sum(axis=1)
     term_3s = predictions.dot(responses[:-1, 0])
@@ -577,7 +576,7 @@ class GreedyKernelInducingPoints(
         unique elements
     :param batch_size: An integer representing the size of the batches of data pairs
         sampled at each iteration for consideration for adding to the coreset
-    :param inverse_approximator: Instance of
+    :param least_square_solver: Instance of
         :class:`coreax.least_squares.RegularisedLeastSquaresSolver`, default value of
         :data:`None` uses :class:`coreax.least_squares.MinimalEuclideanNormSolver`
     """
@@ -587,7 +586,7 @@ class GreedyKernelInducingPoints(
     regularisation_parameter: float = 1e-6
     unique: bool = True
     batch_size: Union[int, None] = None
-    inverse_approximator: Optional[RegularisedLeastSquaresSolver] = None
+    least_square_solver: Optional[RegularisedLeastSquaresSolver] = None
 
     @override
     def reduce(
@@ -616,10 +615,10 @@ class GreedyKernelInducingPoints(
         :return: A refined coresubset and relevant intermediate solver state information
         """
         # Handle default value of None
-        if self.inverse_approximator is None:
-            inverse_approximator = MinimalEuclideanNormSolver(self.random_key)
+        if self.least_square_solver is None:
+            least_square_solver = MinimalEuclideanNormSolver(self.random_key)
         else:
-            inverse_approximator = self.inverse_approximator
+            least_square_solver = self.least_square_solver
 
         # If the initialisation coresubset is too small, pad its nodes up to
         # 'output_size' with -1 valued indices. If it is too large, raise a warning and
@@ -700,7 +699,7 @@ class GreedyKernelInducingPoints(
                 padded_feature_gramian,
                 self.regularisation_parameter,
                 updated_identity,
-                inverse_approximator,
+                least_square_solver,
             )
 
             # Find the optimal replacement coreset index, ensuring we don't pick an
