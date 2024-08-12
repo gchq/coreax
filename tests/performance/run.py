fd0f3d1b08403f6d66bb80eedc413398dd5256d1
rg936672 <162452529+rg936672@users.noreply.github.com>
2024-08-12 10:37:14 +0100

    feat: performance test cases can now be named

--- tests/performance/run.py
+++ tests/performance/run.py
@@ -1,11 +1,18 @@
 """Script to run the performance tests defined in `cases`."""
 
+import argparse
+import json
+
 from cases.basic_coresets import setup_herding, setup_rpc, setup_stein
 
 from coreax.util import speed_comparison_test
 
 if __name__ == "__main__":
-    speed_comparison_test(
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--out_file", default=None)
+    args = parser.parse_args()
+
+    _, timings = speed_comparison_test(
         [
             setup_herding(),
             setup_rpc(),
@@ -15,3 +22,7 @@ if __name__ == "__main__":
         log_results=True,
         check_hash=False,
     )
+
+    if args.out_file is not None:
+        with open(args.out_file, "w", encoding="utf8") as f:
+            json.dump(timings, f)
