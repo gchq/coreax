d650ad46d3f472ece12c138bb89aa6e235f5afcf
pc532627 <138115355+pc532627@users.noreply.github.com>
2024-10-01 10:08:44 +0100

    tests: add analytic kernel herding tests.
    Refs: #786

--- tests/unit/test_solvers.py
+++ tests/unit/test_solvers.py
@@ -524,6 +524,315 @@ class TestKernelHerding(RefinementSolverTest, ExplicitSizeSolverTest):
         expected_state = HerdingState(solver.kernel.gramian_row_mean(dataset))
         assert eqx.tree_equal(state, expected_state)
 
+    def test_kernel_herding_analytic_unique(self) -> None:
+        # pylint: disable=line-too-long
+        r"""
+        Test kernel herding on an analytical example, enforcing a unique coreset.
+
+        In this example, we have data of:
+
+        .. math::
+            x = \begin{pmatrix}
+                0.3 & 0.25 \\
+                0.4 & 0.2 \\
+                0.5 & 0.125
+            \end{pmatrix}
+
+        and choose a ``length_scale`` of :math:`\frac{1}{\sqrt{2}}` to simplify
+        computations with the ``SquaredExponentialKernel``, in particular it becomes:
+
+        .. math::
+            k(x, y) = e^{-||x - y||^2}
+
+        in this example.
+
+        Kernel herding should do as follows:
+            - Compute the Gramian row mean, that is for each data-point :math:`x` and
+              all other data-points :math:`x'`, :math:`\frac{1}{N} \sum_{x'} k(x, x')`
+              where we have :math:`N` data-points in total.
+            - Select the first coreset point :math:`x_{1}` as the data-point where the
+              Gramian row mean is highest.
+            - Compute all future coreset points as
+              :math:`x_{T+1} = \arg\max_{x} \left( \mathbb{E}[k(x, x')] - \frac{1}{T+1}\sum_{t=1}^T k(x, x_t) \right)`
+              where we currently have :math:`T` points in the coreset.
+
+        We ask for a coreset of size 2 in this example. With an empty coreset, we first
+        compute :math:`\mathbb{E}[k(x, x')]` as:
+
+        .. math::
+            \mathbb{E}[k(x, x')] = \frac{1}{3} \cdot \begin{pmatrix}
+                k([0.3, 0.25]', [0.3, 0.25]') + k([0.3, 0.25]', [0.4, 0.2]') + k([0.3, 0.25]', [0.5, 0.125]') \\
+                k([0.4, 0.2]', [0.3, 0.25]') + k([0.4, 0.2]', [0.4, 0.2]') + k([0.4, 0.2]', [0.5, 0.125]') \\
+                k([0.5, 0.125]', [0.3, 0.25]') + k([0.5, 0.125]', [0.4, 0.2]') + k([0.5, 0.125]', [0.5, 0.125]')
+            \end{pmatrix}
+
+        resulting in:
+
+        .. math::
+            \mathbb{E}[k(x, x')] = \begin{pmatrix}
+                0.9778238600172561 \\
+                0.9906914124997632 \\
+                0.9767967388544317
+            \end{pmatrix}
+
+        The largest value in this array is 0.9906914124997632, so we expect the first
+        coreset point to be [0.4, 0.2], that is the data-point at index 1 in the
+        dataset. At this point we have ``coreset_indices`` as [1, ?].
+
+        We then compute the penalty update term
+        :math:`\frac{1}{T+1}\sum_{t=1}^T k(x, x_t)` with :math:`T = 1`:
+
+        .. math::
+            \frac{1}{T+1}\sum_{t=1}^T k(x, x_t) = \frac{1}{2} \cdot \begin{pmatrix}
+                k([0.3, 0.25]', [0.4, 0.2]') \\
+                k([0.4, 0.2]', [0.4, 0.2]') \\
+                k([0.5, 0.125]', [0.4, 0.2]')
+            \end{pmatrix}
+
+        which evaluates to:
+
+        .. math::
+            \frac{1}{T+1}\sum_{t=1}^T k(x, x_t) = \begin{pmatrix}
+                0.4937889002469407 \\
+                0.5 \\
+                0.4922482185027042
+            \end{pmatrix}
+
+        We now select the data-point that maximises
+        :math:`\mathbb{E}[k(x, x')] - \frac{1}{T+1}\sum_{t=1}^T k(x, x_t)`, which
+        evaluates to:
+
+        .. math::
+            \mathbb{E}[k(x, x')] - \frac{1}{T+1}\sum_{t=1}^T k(x, x_t) = \begin{pmatrix}
+                0.9778238600172561 - 0.4937889002469407 \\
+                0.9906914124997632 - 0.5 \\
+                0.9767967388544317 - 0.4922482185027042
+            \end{pmatrix}
+
+        giving a final result of:
+
+        .. math::
+            \mathbb{E}[k(x, x')] - \frac{1}{T+1}\sum_{t=1}^T k(x, x_t) = \begin{pmatrix}
+                0.4840349597703154 \\
+                0.4906914124997632 \\
+                0.4845485203517275
+            \end{pmatrix}
+
+        The largest value in this array is at index 1, which would be to again choose
+        the point [0.4, 0.2] for the coreset. However, in this example we enforce the
+        coreset to be unique, that is not to select the same data-point twice, which
+        means we should take the next highest value in the above result to include in
+        our coreset. This happens to be 0.4845485203517275, the data-point at index 2.
+        This means our final ``coreset_indices`` should be [1, 2].
+
+        Finally, the solver state tracks variables we need not compute repeatedly. In
+        the case of kernel herding, we don't need to recompute
+        :math:`\mathbb{E}[k(x, x')]` at every single step - so the solver state from the
+        coreset reduce method should be set to:
+
+        .. math::
+            \mathbb{E}[k(x, x')] = \begin{pmatrix}
+                0.9778238600172561 \\
+                0.9906914124997632 \\
+                0.9767967388544317
+            \end{pmatrix}
+        """  # noqa: E501
+        # pylint: enable=line-too-long
+        # Setup example data - note we have specifically selected points that are very
+        # close to manipulate the penalty applied for nearby points, and hence enable
+        # a check of unique points using the same data.
+        coreset_size = 2
+        length_scale = 1.0 / jnp.sqrt(2)
+        x = jnp.array(
+            [
+                [0.3, 0.25],
+                [0.4, 0.2],
+                [0.5, 0.125],
+            ]
+        )
+
+        # Define a kernel
+        kernel = SquaredExponentialKernel(length_scale=length_scale)
+
+        # Generate the coreset
+        data = Data(x)
+        solver = KernelHerding(coreset_size=coreset_size, kernel=kernel, unique=True)
+        coreset, solver_state = solver.reduce(data)
+
+        # Define the expected outputs, following the arguments in the docstring
+        expected_coreset_indices = jnp.array([1, 2])
+        expected_gramian_row_mean = jnp.array(
+            [
+                0.9778238600172561,
+                0.9906914124997632,
+                0.9767967388544317,
+            ]
+        )
+
+        # Check output matches expected
+        np.testing.assert_array_equal(
+            coreset.unweighted_indices, expected_coreset_indices
+        )
+        np.testing.assert_array_equal(
+            coreset.coreset.data, data.data[expected_coreset_indices]
+        )
+        np.testing.assert_array_almost_equal(
+            solver_state.gramian_row_mean, expected_gramian_row_mean
+        )
+
+    def test_kernel_herding_analytic_not_unique(self) -> None:
+        # pylint: disable=line-too-long
+        r"""
+        Test kernel herding on an analytical example, enforcing a non-unique coreset.
+
+        In this example, we have data of:
+
+        .. math::
+            x = \begin{pmatrix}
+                0.3 & 0.25 \\
+                0.4 & 0.2 \\
+                0.5 & 0.125
+            \end{pmatrix}
+
+        and choose a ``length_scale`` of :math:`\frac{1}{\sqrt{2}}` to simplify
+        computations with the ``SquaredExponentialKernel``, in particular it becomes:
+
+        .. math::
+            k(x, y) = e^{-||x - y||^2}
+
+        in this example.
+
+        Kernel herding should do as follows:
+            - Compute the Gramian row mean, that is for each data-point :math:`x` and
+              all other data-points :math:`x'`, :math:`\frac{1}{N} \sum_{x'} k(x, x')`
+              where we have :math:`N` data-points in total.
+            - Select the first coreset point :math:`x_{1}` as the data-point where the
+              Gramian row mean is highest.
+            - Compute all future coreset points as
+              :math:`x_{T+1} = \arg\max_{x} \left( \mathbb{E}[k(x, x')] - \frac{1}{T+1}\sum_{t=1}^T k(x, x_t) \right)`
+              where we currently have :math:`T` points in the coreset.
+
+        We ask for a coreset of size 2 in this example. With an empty coreset, we first
+        compute :math:`\mathbb{E}[k(x, x')]` as:
+
+        .. math::
+            \mathbb{E}[k(x, x')] = \frac{1}{3} \cdot \begin{pmatrix}
+                k([0.3, 0.25]', [0.3, 0.25]') + k([0.3, 0.25]', [0.4, 0.2]') + k([0.3, 0.25]', [0.5, 0.125]') \\
+                k([0.4, 0.2]', [0.3, 0.25]') + k([0.4, 0.2]', [0.4, 0.2]') + k([0.4, 0.2]', [0.5, 0.125]') \\
+                k([0.5, 0.125]', [0.3, 0.25]') + k([0.5, 0.125]', [0.4, 0.2]') + k([0.5, 0.125]', [0.5, 0.125]')
+            \end{pmatrix}
+
+        resulting in:
+
+        .. math::
+            \mathbb{E}[k(x, x')] = \begin{pmatrix}
+                0.9778238600172561 \\
+                0.9906914124997632 \\
+                0.9767967388544317
+            \end{pmatrix}
+
+        The largest value in this array is 0.9906914124997632, so we expect the first
+        coreset point to be [0.4, 0.2], that is the data-point at index 1 in the
+        dataset. At this point we have ``coreset_indices`` as [1, ?].
+
+        We then compute the penalty update term
+        :math:`\frac{1}{T+1}\sum_{t=1}^T k(x, x_t)` with :math:`T = 1`:
+
+        .. math::
+            \frac{1}{T+1}\sum_{t=1}^T k(x, x_t) = \frac{1}{2} \cdot \begin{pmatrix}
+                k([0.3, 0.25]', [0.4, 0.2]') \\
+                k([0.4, 0.2]', [0.4, 0.2]') \\
+                k([0.5, 0.125]', [0.4, 0.2]')
+            \end{pmatrix}
+
+        which evaluates to:
+
+        .. math::
+            \frac{1}{T+1}\sum_{t=1}^T k(x, x_t) = \begin{pmatrix}
+                0.4937889002469407 \\
+                0.5 \\
+                0.4922482185027042
+            \end{pmatrix}
+
+        We now select the data-point that maximises
+        :math:`\mathbb{E}[k(x, x')] - \frac{1}{T+1}\sum_{t=1}^T k(x, x_t)`, which
+        evaluates to:
+
+        .. math::
+            \mathbb{E}[k(x, x')] - \frac{1}{T+1}\sum_{t=1}^T k(x, x_t) = \begin{pmatrix}
+                0.9778238600172561 - 0.4937889002469407 \\
+                0.9906914124997632 - 0.5 \\
+                0.9767967388544317 - 0.4922482185027042
+            \end{pmatrix}
+
+        giving a final result of:
+
+        .. math::
+            \mathbb{E}[k(x, x')] - \frac{1}{T+1}\sum_{t=1}^T k(x, x_t) = \begin{pmatrix}
+                0.4840349597703154 \\
+                0.4906914124997632 \\
+                0.4845485203517275
+            \end{pmatrix}
+
+        The largest value in this array is at index 1, which would be to again choose
+        the point [0.4, 0.2] for the coreset. Since we don't enforce the coreset points
+        selected to be unique here, our final ``coreset_indices`` should be [1, 1].
+
+        Finally, the solver state tracks variables we need not compute repeatedly. In
+        the case of kernel herding, we don't need to recompute
+        :math:`\mathbb{E}[k(x, x')]` at every single step - so the solver state from the
+        coreset reduce method should be set to:
+
+        .. math::
+            \mathbb{E}[k(x, x')] = \begin{pmatrix}
+                0.9778238600172561 \\
+                0.9906914124997632 \\
+                0.9767967388544317
+            \end{pmatrix}
+        """  # noqa: E501
+        # pylint: enable=line-too-long
+        # Setup example data - note we have specifically selected points that are very
+        # close to manipulate the penalty applied for nearby points, and hence enable
+        # a check of unique points using the same data.
+        coreset_size = 2
+        length_scale = 1.0 / jnp.sqrt(2)
+        x = jnp.array(
+            [
+                [0.3, 0.25],
+                [0.4, 0.2],
+                [0.5, 0.125],
+            ]
+        )
+
+        # Define a kernel
+        kernel = SquaredExponentialKernel(length_scale=length_scale)
+
+        # Generate the coreset
+        data = Data(x)
+        solver = KernelHerding(coreset_size=coreset_size, kernel=kernel, unique=False)
+        coreset, solver_state = solver.reduce(data)
+
+        # Define the expected outputs, following the arguments in the docstring
+        expected_coreset_indices = jnp.array([1, 1])
+        expected_gramian_row_mean = jnp.array(
+            [
+                0.9778238600172561,
+                0.9906914124997632,
+                0.9767967388544317,
+            ]
+        )
+
+        # Check output matches expected
+        np.testing.assert_array_equal(
+            coreset.unweighted_indices, expected_coreset_indices
+        )
+        np.testing.assert_array_equal(
+            coreset.coreset.data, data.data[expected_coreset_indices]
+        )
+        np.testing.assert_array_almost_equal(
+            solver_state.gramian_row_mean, expected_gramian_row_mean
+        )
+
 
 class TestRandomSample(ExplicitSizeSolverTest):
     """Test cases for :class:`coreax.solvers.coresubset.RandomSample`."""
