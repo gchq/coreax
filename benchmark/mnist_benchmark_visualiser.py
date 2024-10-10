055206b099869e296e6a793e0f56f5b7e566d3f8
qh681248 <181246904+qh681248@users.noreply.github.com>
2024-10-10 10:41:00 +0100

    Added copyright and documentation

--- benchmark/mnist_benchmark_visualiser.py
+++ benchmark/mnist_benchmark_visualiser.py
@@ -1,8 +1,25 @@
+# © Crown Copyright GCHQ
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Visualise the results of ``mnist_benchmark.py``."""
+
 import json
+
 from matplotlib import pyplot as plt
 
 # Open the JSON file
-with open('benchmark_results.json', 'r') as file:
+with open("benchmark_results.json", "r", encoding="utf-8") as file:
     # Load the JSON data into a Python object
     data_by_solver = json.load(file)
 
@@ -11,12 +28,12 @@ with open('benchmark_results.json', 'r') as file:
 plt.figure(figsize=(12, 6))
 
 for algo, data in data_by_solver.items():
-    plt.plot(data['coreset_size'], data['accuracy'], 'o-', label=algo)
+    plt.plot(data["coreset_size"], data["accuracy"], "o-", label=algo)
 
-plt.xlabel('Iteration')
-plt.ylabel('Performance Metric')
-plt.title('Algorithm Performance Across Iterations')
+plt.xlabel("Iteration")
+plt.ylabel("Performance Metric")
+plt.title("Algorithm Performance Across Iterations")
 plt.legend()
-plt.grid(True, linestyle='--', alpha=0.7)
+plt.grid(True, linestyle="--", alpha=0.7)
 
 plt.show()
