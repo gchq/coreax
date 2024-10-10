9f799bfaf22c8a5134db8f90551a9268106de021
qh681248 <181246904+qh681248@users.noreply.github.com>
2024-10-10 14:00:19 +0100

    Added documentation on benchmark/__init__.py

--- benchmark/__init__.py
+++ benchmark/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Different benchmarking tasks and algorithms"""
+"""Different benchmarking tests to compare performance of different algorithms."""
