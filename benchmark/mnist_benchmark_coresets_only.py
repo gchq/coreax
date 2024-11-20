28ec9775140e1dd09aa4e8130eec685fa6694b48
qh681248 <181246904+qh681248@users.noreply.github.com>
2024-11-20 16:58:42 +0000

    chore: Update docstrings for mnist_benchmark_coresets_only.py

--- benchmark/mnist_benchmark_coresets_only.py
+++ benchmark/mnist_benchmark_coresets_only.py
@@ -24,187 +24,24 @@ The benchmarking process follows these steps:
    before applying coreset algorithms.
 4. Generate coresets of different sizes using various coreset algorithms and record the
    time taken.
+
+The benchmark is run on amazon g4dn.12xlarge instance with 4 nvidia t4 tensor core
+GPUs, 48 virtual CPUs and 192 GiB memory.
 """
 
 import json
 import os
 import time
-from typing import Callable
 
 import equinox as eqx
 import jax
-import jax.numpy as jnp
-import numpy as np
-import torchvision
-from torch.utils.data import DataLoader, Dataset
-from torchvision import transforms
-
-from coreax import Data
-from coreax.kernels import SquaredExponentialKernel, SteinKernel, median_heuristic
-from coreax.score_matching import KernelDensityMatching
-from coreax.solvers import (
-    KernelHerding,
-    MapReduce,
-    RandomSample,
-    RPCholesky,
-    SteinThinning,
+from mnist_benchmark import (
+    initialise_solvers,
+    pca,
+    prepare_datasets,
 )
 
-
-# Convert PyTorch dataset to JAX arrays
-def convert_to_jax_arrays(pytorch_data: Dataset) -> tuple[jnp.ndarray, jnp.ndarray]:
-    """
-    Convert a PyTorch dataset to JAX arrays.
-
-    :param pytorch_data: PyTorch dataset to convert.
-    :return: Tuple of JAX arrays (data, targets).
-    """
-    # Load all data in one batch
-    data_loader = DataLoader(pytorch_data, batch_size=len(pytorch_data))
-    # Grab the first batch, which is all data
-    _data, _targets = next(iter(data_loader))
-    # Convert to NumPy first, then JAX array
-    data_jax = jnp.array(_data.numpy())
-    targets_jax = jnp.array(_targets.numpy())
-    return data_jax, targets_jax
-
-
-def pca(x: jnp.ndarray, n_components: int = 16) -> jnp.ndarray:
-    """
-    Perform Principal Component Analysis (PCA) on the dataset x.
-
-    This function computes the principal components of the input data matrix
-    and returns the projected data.
-
-    :param x: The input data matrix of shape (n_samples, n_features).
-    :param n_components: The number of principal components to return.
-    :return: The projected data of shape (n_samples, n_components).
-    """
-    # Center the data
-    x_centred = x - jnp.mean(x, axis=0)
-
-    # Compute the covariance matrix
-    cov_matrix = jnp.cov(x_centred.T)
-
-    # Compute eigenvalues and eigenvectors
-    eigenvalues, eigenvectors = jnp.linalg.eigh(cov_matrix)
-
-    # Sort eigenvectors by descending eigenvalues
-    _idx = jnp.argsort(eigenvalues)[::-1]
-    eigenvectors = eigenvectors[:, _idx]
-
-    # Select top n_components eigenvectors
-    components = eigenvectors[:, :n_components]
-
-    # Project the data onto the new subspace
-    x_pca = jnp.dot(x_centred, components)
-
-    return x_pca
-
-
-def prepare_datasets() -> tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]:
-    """
-    Prepare and return training and test datasets.
-
-    :return: A tuple containing training and test datasets in JAX arrays:
-             (train_data_jax, train_targets_jax, test_data_jax, test_targets_jax).
-    """
-    transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Lambda(lambda x: x.view(-1))]
-    )
-    train_dataset = torchvision.datasets.MNIST(
-        root="./data", train=True, download=True, transform=transform
-    )
-    test_dataset = torchvision.datasets.MNIST(
-        root="./data", train=False, download=True, transform=transform
-    )
-
-    train_data_jax, train_targets_jax = convert_to_jax_arrays(train_dataset)
-    test_data_jax, test_targets_jax = convert_to_jax_arrays(test_dataset)
-
-    return train_data_jax, train_targets_jax, test_data_jax, test_targets_jax
-
-
-def initialise_solvers(train_data_pca: Data, key: jax.random.PRNGKey) -> list[Callable]:
-    """
-    Initialise and return a list of solvers for various coreset algorithms.
-
-    Set up solvers for Kernel Herding, Stein Thinning, Random Sampling, and Randomised
-    Cholesky methods. Each solver has different parameter requirements. Some solvers
-    can utilise MapReduce, while others cannot,and some require specific kernels.
-    This setup allows them to be called by passing only the coreset size,
-    enabling easy integration in a loop for benchmarking.
-
-    :param train_data_pca: The PCA-transformed training data used for
-                           length scale estimation for ``SquareExponentialKernel``.
-    :param key: The random key for initialising random solvers.
-
-    :return: A list of solvers functions for different coreset algorithms.
-    """
-    # Set up kernel using median heuristic
-    num_data_points = len(train_data_pca)
-    num_samples_length_scale = min(num_data_points, 300)
-    random_seed = 45
-    generator = np.random.default_rng(random_seed)
-    idx = generator.choice(num_data_points, num_samples_length_scale, replace=False)
-    length_scale = median_heuristic(train_data_pca[idx])
-    kernel = SquaredExponentialKernel(length_scale=length_scale)
-
-    def _get_herding_solver(_size: int) -> MapReduce:
-        """
-        Set up KernelHerding to use ``MapReduce``.
-
-        Create a KernelHerding solver with the specified size and return
-        it along with a MapReduce object for reducing a large dataset like
-        MNIST dataset.
-
-        :param _size: The size of the coreset to be generated.
-        :return: A tuple containing the solver name and the MapReduce solver.
-        """
-        herding_solver = KernelHerding(_size, kernel)
-        return MapReduce(herding_solver, leaf_size=3 * _size)
-
-    def _get_stein_solver(_size: int) -> MapReduce:
-        """
-        Set up Stein Thinning to use ``MapReduce``.
-
-        Create a SteinThinning solver with the specified  coreset size,
-        using ``KernelDensityMatching`` score function for matching on
-        a subset of the dataset.
-
-        :param _size: The size of the coreset to be generated.
-        :return: A tuple containing the solver name and the MapReduce solver.
-        """
-        # Generate small dataset for ScoreMatching for Stein Kernel
-
-        score_function = KernelDensityMatching(length_scale=length_scale).match(
-            train_data_pca[idx]
-        )
-        stein_kernel = SteinKernel(kernel, score_function)
-        stein_solver = SteinThinning(coreset_size=_size, kernel=stein_kernel)
-        return MapReduce(stein_solver, leaf_size=3 * _size)
-
-    def _get_random_solver(_size: int) -> RandomSample:
-        """
-        Set up Random Sampling to generate a coreset.
-
-        :param _size: The size of the coreset to be generated.
-        :return: A tuple containing the solver name and the RandomSample solver.
-        """
-        random_solver = RandomSample(_size, key)
-        return random_solver
-
-    def _get_rp_solver(_size: int) -> RPCholesky:
-        """
-        Set up Randomised Cholesky solver.
-
-        :param _size: The size of the coreset to be generated.
-        :return: A tuple containing the solver name and the RPCholesky solver.
-        """
-        rp_solver = RPCholesky(coreset_size=_size, kernel=kernel, random_key=key)
-        return rp_solver
-
-    return [_get_random_solver, _get_rp_solver, _get_herding_solver, _get_stein_solver]
+from coreax import Data
 
 
 def save_results(results: dict) -> None:
@@ -215,21 +52,21 @@ def save_results(results: dict) -> None:
                     {
                         "algorithm_name": {
                             "coreset_size_1": {
-                                "run_1": accuracy_value,
-                                "run_2": accuracy_value,
+                                "run_1": time_taken,
+                                "run_2": time_taken,
                                 ...
                             },
                             "coreset_size_2": {
-                                "run_1": accuracy_value,
-                                "run_2": accuracy_value,
+                                "run_1": time_taken,
+                                "run_2": time_taken,
                                 ...
                             },
                             ...
                         },
                         "another_algorithm_name": {
                             "coreset_size_1": {
-                                "run_1": accuracy_value,
-                                "run_2": accuracy_value,
+                                "run_1": time_taken,
+                                "run_2": time_taken,
                                 ...
                             },
                             ...
@@ -237,7 +74,7 @@ def save_results(results: dict) -> None:
                         ...
                     }
                     Each algorithm contains coreset sizes as keys, with values being
-                    dictionaries of accuracy results from different runs.
+                    dictionaries of time taken from different runs.
     """
     base_dir = os.path.dirname(os.path.abspath(__file__))
     file_name = "mnist_time_results.json"
@@ -247,7 +84,6 @@ def save_results(results: dict) -> None:
     print(f"Data has been saved to {file_name}")
 
 
-# pylint: disable=too-many-locals
 def main() -> None:
     """
     Perform the benchmark for multiple solvers, coreset sizes, and random seeds.
@@ -256,12 +92,9 @@ def main() -> None:
     1. Prepare and load the MNIST datasets (training and test).
     2. Perform dimensionality reduction on the training data using PCA.
     3. Initialise solvers for data reduction.
-    4. For each solver and coreset size, reduce the dataset and train the model
-       on the reduced set.
-    5. Train the model and evaluate its performance on the test set.
-    6. Save the results, which include test accuracy for each solver and coreset size.
+    4. For each solver and coreset size, reduce the dataset and store the time taken.
     """
-    (train_data_jax, _, _, _) = prepare_datasets()
+    train_data_jax, _, _, _ = prepare_datasets()
     train_data_pca = Data(pca(train_data_jax))
 
     coreset_times = {}
