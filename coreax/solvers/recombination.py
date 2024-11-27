fix: numerical errors in GPU recombination
This uses a much simpler fix proposed by @tc85324 - the null space basis
update was incorrect.

This commit effectively reverts the last two commits.

Refs: #853

coreax/solvers/recombination.py
@@ -141,7 +141,6 @@ class _EliminationState(NamedTuple):
     weights: Shaped[Array, " n"]
     nodes: Shaped[Array, "n m"]
     iteration: int
-    eliminated: Shaped[Array, " n"]
 
 
 class CaratheodoryRecombination(RecombinationSolver[Data, None]):
@@ -193,10 +192,6 @@ class CaratheodoryRecombination(RecombinationSolver[Data, None]):
             safe_push_forward_nodes, self.rcond
         )
 
-        # Array to keep track of which nodes have been eliminated (not described by
-        # the base algorithm, but needed to combat numerical errors on GPU systems)
-        eliminated_array = jnp.zeros_like(weights, dtype=bool)
-
         def _eliminate_cond(state: _EliminationState) -> Bool[Array, ""]:
             """
             If to continue the iterative Gaussian-Elimination procedure.
@@ -241,14 +236,13 @@ class CaratheodoryRecombination(RecombinationSolver[Data, None]):
             # - `updated_weights` -> \underline\Beta^{(i)}
             # - `null_space_basis_update` -> d_{l+1}^{(i)}\phi_1^{(i-1)}
             # - `updated_null_space_basis` -> \Psi^{(i))
-            _weights, null_space_basis, basis_index, eliminated = state
+            _weights, null_space_basis, basis_index = state
             basis_vector = null_space_basis[basis_index]
             # Equation 3: Select the weight to eliminate.
             elimination_condition = jnp.where(
                 basis_vector > 0, _weights / basis_vector, jnp.inf
             )
             elimination_index = jnp.argmin(elimination_condition)
-            eliminated = eliminated.at[elimination_index].set(True)
             elimination_rescaling_factor = elimination_condition[elimination_index]
             # Equation 4: Eliminate the selected weight and redistribute its mass.
             # NOTE: Equation 5 is implicit from Equation 4 and is performed outside
@@ -262,21 +256,14 @@ class CaratheodoryRecombination(RecombinationSolver[Data, None]):
                 axes=0,
             )
             updated_null_space_basis = null_space_basis - null_space_basis_update
-            updated_null_space_basis = updated_null_space_basis.at[basis_index].set(0)
-            # Ensure eliminated rows *stay* eliminated.
-            # On GPU systems, small numerical errors can cause previously eliminated
-            # rows to have small but positive values in the basis vector. This line
-            # ensures that this does not happen.
-            updated_null_space_basis = jnp.where(
-                jnp.expand_dims(eliminated, 0), 0, updated_null_space_basis
-            )
+            updated_null_space_basis = updated_null_space_basis.at[
+                :, elimination_index
+            ].set(0)
             return _EliminationState(
-                updated_weights, updated_null_space_basis, basis_index + 1, eliminated
+                updated_weights, updated_null_space_basis, basis_index + 1
             )
 
-        in_state = _EliminationState(
-            safe_weights, largest_null_space_basis, 0, eliminated_array
-        )
+        in_state = _EliminationState(safe_weights, largest_null_space_basis, 0)
         out_weights, *_ = jax.lax.while_loop(_eliminate_cond, _eliminate, in_state)
         coresubset_nodes = _coresubset_nodes(
             safe_push_forward_nodes,